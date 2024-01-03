3435ae02315f90efe12322d76e62c72baddfe415
yoon02 <sparkling_yo@naver.com>
2024-01-03 13:30:29 +0900

    first commit

---
+++ src/main/java/com/objects/marketbridge/MarketbridgeApplication.java
@@ -0,0 +1,13 @@
+package com.objects.marketbridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketbridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketbridgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/objects/marketbridge/MarketbridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.objects.marketbridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketbridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
