74e500268a4b9206def61f0d96930894a5ced860
Juho Lee <juho3416@gmail.com>
2024-01-06 23:13:16 +0900

    [:sparkles: | #13 | Feature] ProductImage 연관관계매핑
    ProductImage 연관관계매핑(단방향) 완료

--- src/main/java/com/objects/marketbridge/domain/ProductImage.java
+++ src/main/java/com/objects/marketbridge/domain/ProductImage.java
@@ -1,9 +1,6 @@
 package com.objects.marketbridge.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,13 +16,16 @@ public class ProductImage extends BaseEntity{
     @Column(name = "product_image_id")
     private Long id;
 
-    // TODO
-    private Long productId;
-    // TODO
-    private Long imageId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product productId;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "image_id")
+    private Image imageId;
 
     @Builder
-    private ProductImage(Long productId, Long imageId) {
+    private ProductImage(Product productId, Image imageId) {
         this.productId = productId;
         this.imageId = imageId;
     }
