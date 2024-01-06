632b125995a1c7bb26850ee24d358b204666159d
Juho Lee <juho3416@gmail.com>
2024-01-06 23:55:34 +0900

    [:sparkles: | #13 | Feature] SellerProduct 연관관계매핑
    SellerProduct 연관관계매핑(단방향) 완료

--- src/main/java/com/objects/marketbridge/domain/SellerProduct.java
+++ src/main/java/com/objects/marketbridge/domain/SellerProduct.java
@@ -1,9 +1,6 @@
 package com.objects.marketbridge.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -18,14 +15,18 @@ public class SellerProduct extends BaseEntity{
     @GeneratedValue
     @Column(name = "seller_product_id")
     private Long id;
-    // TODO
-    private Long userId;
-    // TODO
-    private Long productId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "seller_id")
+    private Seller sellerId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product productId;
 
     @Builder
-    private SellerProduct(Long userId, Long productId) {
-        this.userId = userId;
+    private SellerProduct(Seller sellerId, Product productId) {
+        this.sellerId = sellerId;
         this.productId = productId;
     }
 }
