abac62a33f92809809a56fa4311b106a90a762a5
Jhung Minwoo <minwoorich96@gmail.com>
2024-01-09 12:46:14 +0900

    [ :sparkles: | #27 | fix ] Order 도메인 기본적인 세팅 완료 (추가 수정)
    - OrderService 가 service/port 에 들어가 있는걸 service로 빼냈음

--- src/main/java/com/objects/marketbridge/domain/order/service/OrderService.java
+++ src/main/java/com/objects/marketbridge/domain/order/service/OrderService.java
@@ -1,4 +1,4 @@
-package com.objects.marketbridge.domain.order.service.port;
+package com.objects.marketbridge.domain.order.service;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
