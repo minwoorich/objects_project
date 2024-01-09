f3d7f7d26bdbb65b35066ca489a4c0e2d7fbb62e
Jhung Minwoo <minwoorich96@gmail.com>
2024-01-09 22:02:45 +0900

    [ :sparkles: | #27 | feature ] /orders/direct/checkout 컨트롤러메서드 생성
    - 바로구매 눌렀을때 쏘는 API 에 대한 컨트롤러 메서드
    - 미완성

--- src/main/java/com/objects/marketbridge/domain/order/controller/OrderController.java
+++ src/main/java/com/objects/marketbridge/domain/order/controller/OrderController.java
@@ -1,25 +1,29 @@
 package com.objects.marketbridge.domain.order.controller;
 
-import com.objects.marketbridge.domain.order.dto.OrderCreate;
+import com.objects.marketbridge.domain.order.controller.response.CheckoutResponse;
+import com.objects.marketbridge.domain.order.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class OrderController {
-    @PostMapping("/orders/direct/checkout")
-    public ResponseEntity<OrderResponse> directOrder(@RequestBody OrderCreate order) {
+
+    private final OrderService orderService;
+
+    @GetMapping("/orders/direct/checkout")
+    public ResponseEntity<CheckoutResponse> directInitOrder(
+            @RequestParam List<String> items) {
+
         return null;
-    }
-
-
-
-
-
-    static class OrderResponse{
 
     }
+
+
+
 }
