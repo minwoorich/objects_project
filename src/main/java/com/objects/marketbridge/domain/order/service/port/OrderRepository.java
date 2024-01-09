74f2c9397bc9b36d298aaa092444431a84854c5e
sunmok <tjsahr9191@naver.com>
2024-01-09 18:26:59 +0900

    [ :sparkle: |#34|Feature] 주문 조회, 주문 저장 및 테스트
    주문 아이디로 주문 조회, 주문 저장에 대한 테스트

--- src/main/java/com/objects/marketbridge/domain/order/service/port/OrderRepository.java
+++ src/main/java/com/objects/marketbridge/domain/order/service/port/OrderRepository.java
@@ -1,7 +1,13 @@
 package com.objects.marketbridge.domain.order.service.port;
 
-import com.objects.marketbridge.domain.order.repository.OrderJpaRepository;
+
+import com.objects.marketbridge.domain.model.ProdOrder;
+
+import java.util.Optional;
 
 public interface OrderRepository {
 
+    Optional<ProdOrder> findById(Long orderId);
+
+    ProdOrder save(ProdOrder order);
 }
