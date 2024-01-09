4899a476d28a6af0b9e7858ced812481d84a8f5d
sunmok <tjsahr9191@naver.com>
2024-01-09 14:58:13 +0900

    서버 뜬 커밋

--- src/main/java/com/objects/marketbridge/domain/order/repository/OrderJpaRepository.java
+++ src/main/java/com/objects/marketbridge/domain/order/repository/OrderJpaRepository.java
@@ -6,8 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface OrderJpaRepository extends JpaRepository<ProdOrder, Long> {
-    Optional<ProdOrder> findById(Long id);
-    ProdOrder getById(Long id);
-
-    Optional<ProdOrder> findByOrderNo();
+//    Optional<ProdOrder> findById(Long id);
+//    ProdOrder getById(Long id);
+//
+//    Optional<ProdOrder> findByOrderNo();
 }
