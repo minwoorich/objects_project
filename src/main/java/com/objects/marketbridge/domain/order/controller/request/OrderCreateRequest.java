537126cce5956d0a05ca05dd173dfa8fda9f4be7
Jhung Minwoo <minwoorich96@gmail.com>
2024-01-09 21:56:56 +0900

    [ :sparkles: | #27 | feature ] 주문에 대한 request, response dto 생성
    - 각각 controller/request, controller/response 에 생성

---
+++ src/main/java/com/objects/marketbridge/domain/order/controller/request/OrderCreateRequest.java
@@ -0,0 +1,32 @@
+package com.objects.marketbridge.domain.order.controller.request;
+
+import com.objects.marketbridge.domain.order.dto.OrderCreate;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class OrderCreateRequest {
+
+    @NotNull
+    private final Long productId;
+
+    @NotNull
+    private final Long productOptionId;
+
+    @Min(1)
+    private final Integer amount;
+
+    @Builder
+    public OrderCreateRequest(Long productId, Long productOptionId, Integer amount) {
+        this.productId = productId;
+        this.productOptionId = productOptionId;
+        this.amount = amount;
+    }
+
+    public OrderCreate to() {
+        return OrderCreate.builder()
+                .
+    }
+}
