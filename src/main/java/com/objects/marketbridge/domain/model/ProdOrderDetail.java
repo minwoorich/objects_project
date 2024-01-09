39303f5daa115d9358c8f6cb3391b04b0361f478
Jhung Minwoo <minwoorich96@gmail.com>
2024-01-09 18:22:08 +0900

    [ :wrench: | #27 | fix ] ProdOrderDetail,컬럼 수정
    없는 컬럼 채워넣었음

--- src/main/java/com/objects/marketbridge/domain/model/ProdOrderDetail.java
+++ src/main/java/com/objects/marketbridge/domain/model/ProdOrderDetail.java
@@ -6,6 +6,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -24,15 +26,35 @@ public class ProdOrderDetail extends BaseEntity{
     @JoinColumn(name = "product_id")
     private Product product;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "coupon_id")
+    private Coupon coupon;
+
+    private Integer quantity;
+
+    private Integer price;
+
     private String statusCode;
 
-    private Long price;
+    private LocalDateTime deliveredDate;
+
+    private Integer usedPoint;
+
+    private String reason;
+
+    private LocalDateTime cancelledAt;
 
     @Builder
-    private ProdOrderDetail(ProdOrder prodOrder, Product product, String statusCode, Long price) {
+    private ProdOrderDetail(ProdOrder prodOrder, Product product, Coupon coupon, Integer quantity, Integer price, String statusCode, LocalDateTime deliveredDate, Integer usedPoint, String reason, LocalDateTime cancelledAt) {
         this.prodOrder = prodOrder;
         this.product = product;
-        this.statusCode = statusCode;
+        this.coupon = coupon;
+        this.quantity = quantity;
         this.price = price;
+        this.statusCode = statusCode;
+        this.deliveredDate = deliveredDate;
+        this.usedPoint = usedPoint;
+        this.reason = reason;
+        this.cancelledAt = cancelledAt;
     }
 }
