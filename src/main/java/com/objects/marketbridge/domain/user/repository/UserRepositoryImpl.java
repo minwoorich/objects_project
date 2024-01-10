1f8d90266dce4991e8690e12cc3c56fe170824bc
pyoung <pmmh9395@gmail.com>
2024-01-10 12:39:13 +0900

    [ :wrench: | #46 | fix ] @Repository 어노테이션 추가

--- src/main/java/com/objects/marketbridge/domain/user/repository/UserRepositoryImpl.java
+++ src/main/java/com/objects/marketbridge/domain/user/repository/UserRepositoryImpl.java
@@ -1,4 +1,7 @@
 package com.objects.marketbridge.domain.user.repository;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class UserRepositoryImpl implements UserRepositoryCustom {
 }
