b916cc82f2493e67a1062ff5168c5285a1e16945
Jhung Minwoo <minwoorich96@gmail.com>
2024-01-11 00:25:10 +0900

    [ :sparkles: | #27 | feature ] OrderService : create() 생성
    - 주문 생성 하는 메서드
    
    //TODO
    - 재고 차감
    - 포인트 사용시 포인트 차감
    - 쿠폰 사용시 쿠폰 차감
    - 주문 상태 변경
    - ,,,,

--- src/main/java/com/objects/marketbridge/domain/order/service/OrderService.java
+++ src/main/java/com/objects/marketbridge/domain/order/service/OrderService.java
@@ -1,10 +1,24 @@
 package com.objects.marketbridge.domain.order.service;
 
+import com.objects.marketbridge.domain.model.ProdOrderDetail;
+import com.objects.marketbridge.domain.order.dto.CreateOrderDto;
+import com.objects.marketbridge.domain.order.service.port.OrderDetailRepository;
+import com.objects.marketbridge.domain.order.service.port.OrderRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class OrderService {
 
+    private final OrderRepository orderRepository;
+    private final OrderDetailRepository orderDetailRepository;
+
+    @Transactional
+    public void create(CreateOrderDto orderDto) {
+        ProdOrderDetail prodOrderDetail = ProdOrderDetail.from(orderDto);
+        orderDetailRepository.save(prodOrderDetail);
+    }
 }
