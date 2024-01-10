345d75c700f2fa67220af2a22a65c18fb87c61fd
sunmok <tjsahr9191@naver.com>
2024-01-10 21:22:05 +0900

    [ :white_check_mark: | #34 | test ] 상품상세 넣기 테스트

--- src/test/java/com/objects/marketbridge/domain/order/service/port/OrderDetailRepositoryTest.java
+++ src/test/java/com/objects/marketbridge/domain/order/service/port/OrderDetailRepositoryTest.java
@@ -4,7 +4,6 @@ import com.objects.marketbridge.domain.model.ProdOrder;
 import com.objects.marketbridge.domain.model.ProdOrderDetail;
 import com.objects.marketbridge.domain.model.StatusCodeType;
 import jakarta.persistence.EntityManager;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,8 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 @SpringBootTest
@@ -34,12 +32,12 @@ class OrderDetailRepositoryTest {
     @DisplayName("특정 주문에 해당하는 주문 상세의 상태 코드를 한번에 바꾼다.")
     public void changeAllType() {
         // given
-        String givenType = StatusCodeType.PAYMENT_COMPLETED.getCode();
-        String changeType = StatusCodeType.ORDER_CANCEL.getCode();
+        String givenCodeType = StatusCodeType.PAYMENT_COMPLETED.getCode();
+        String changeCodeType = StatusCodeType.ORDER_CANCEL.getCode();
 
-        ProdOrderDetail orderDetail1 = createOrderDetail(givenType);
-        ProdOrderDetail orderDetail2 = createOrderDetail(givenType);
-        ProdOrderDetail orderDetail3 = createOrderDetail(givenType);
+        ProdOrderDetail orderDetail1 = createOrderDetail_type(givenCodeType);
+        ProdOrderDetail orderDetail2 = createOrderDetail_type(givenCodeType);
+        ProdOrderDetail orderDetail3 = createOrderDetail_type(givenCodeType);
 
         ProdOrder order = ProdOrder.builder().build();
         order.addOrderDetail(orderDetail1);
@@ -47,9 +45,10 @@ class OrderDetailRepositoryTest {
         order.addOrderDetail(orderDetail3);
 
         ProdOrder savedOrder = orderRepository.save(order);
+        Long orderId = savedOrder.getId();
 
         // when
-        int result = orderDetailRepository.changeAllType(savedOrder.getId(), changeType);
+        int result = orderDetailRepository.changeAllType(orderId, changeCodeType);
 
         // then
         assertThat(result).isEqualTo(3);
@@ -60,9 +59,9 @@ class OrderDetailRepositoryTest {
     public void saveAll() {
         // given
         String givenType = StatusCodeType.PAYMENT_COMPLETED.getCode();
-        ProdOrderDetail orderDetail1 = createOrderDetail(givenType);
-        ProdOrderDetail orderDetail2 = createOrderDetail(givenType);
-        ProdOrderDetail orderDetail3 = createOrderDetail(givenType);
+        ProdOrderDetail orderDetail1 = createOrderDetail_type(givenType);
+        ProdOrderDetail orderDetail2 = createOrderDetail_type(givenType);
+        ProdOrderDetail orderDetail3 = createOrderDetail_type(givenType);
         List<ProdOrderDetail> orderDetails = List.of(orderDetail1, orderDetail2, orderDetail3);
 
         // when
@@ -72,9 +71,41 @@ class OrderDetailRepositoryTest {
         assertThat(savedOrderDetails.size()).isEqualTo(3);
     }
 
-    private ProdOrderDetail createOrderDetail(String code) {
+    @Test
+    @DisplayName("주문 상세에 이유 넣기")
+    public void addReason() {
+        // given
+        String reason = "상품이 맘에들지 않아요";
+        ProdOrderDetail orderDetail1 = ProdOrderDetail.builder().build();
+        ProdOrderDetail orderDetail2 = ProdOrderDetail.builder().build();
+        ProdOrderDetail orderDetail3 = ProdOrderDetail.builder().build();
+
+        ProdOrder order = ProdOrder.builder().build();
+        order.addOrderDetail(orderDetail1);
+        order.addOrderDetail(orderDetail2);
+        order.addOrderDetail(orderDetail3);
+
+        ProdOrder savedOrder = orderRepository.save(order);
+        Long orderId = savedOrder.getId();
+
+        // when
+        orderDetailRepository.addReason(orderId, reason);
+
+        // then
+        String savedReason = getReason(orderId);
+        assertThat(reason).isEqualTo(savedReason);
+    }
+
+    private String getReason(Long orderId) {
+        List<ProdOrderDetail> prodOrderDetails = orderRepository.findById(orderId).get().getProdOrderDetails();
+        return prodOrderDetails.get(0).getReason();
+    }
+
+    private ProdOrderDetail createOrderDetail_type(String code) {
         return ProdOrderDetail.builder()
                 .statusCode(code)
                 .build();
     }
+
+
 }
