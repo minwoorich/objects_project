03e1a32a371ba94bee5e2e7c94f5058c1f55c1df
pyoung <pmmh9395@gmail.com>
2024-01-11 09:29:45 +0900

    [ :wrench: | #46 | fix ] CreateUser  DTO에 벨리데이션 추가
    회원가입에 필요한 dto에 벨리데이션 추가

--- src/main/java/com/objects/marketbridge/domain/user/dto/CreateUser.java
+++ src/main/java/com/objects/marketbridge/domain/user/dto/CreateUser.java
@@ -1,6 +1,7 @@
 package com.objects.marketbridge.domain.user.dto;
 
 
+import jakarta.validation.constraints.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,11 +9,21 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class CreateUser {
-   private String email;
-   private String password;
-   private String name;
-   private String phoneNo;
-   private Boolean isAgree;
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "이메일 필수값 입니다.")
+    private String email;
+
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 6, message = "Password should have at least 6 characters")
+    private String password;
+
+    @NotBlank(message = "Name cannot be blank")
+    private String name;
+
+    @Pattern(regexp = "\\d{3}-\\d{3,4}-\\d{4}", message = "Phone number should have the format XXX-XXXX-XXXX")
+    private String phoneNo;
+
+    private Boolean isAgree;
 
 
     @Builder
