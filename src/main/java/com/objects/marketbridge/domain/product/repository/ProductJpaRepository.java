9d58e8b24ba9ee2766482b62e67e05211dfb8a98
Jhung Minwoo <minwoorich96@gmail.com>
2024-01-12 02:40:15 +0900

    [ :sparkles: | #27 | feature ] Product 도메인의 레포지토리 파일들 추가

--- src/main/java/com/objects/marketbridge/domain/product/repository/ProductJpaRepository.java
+++ src/main/java/com/objects/marketbridge/domain/product/repository/ProductJpaRepository.java
@@ -3,5 +3,5 @@ package com.objects.marketbridge.domain.product.repository;
 import com.objects.marketbridge.domain.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductJpaRepository extends JpaRepository<Product,Long> {
+public interface ProductJpaRepository extends JpaRepository<Product, Long> {
 }
