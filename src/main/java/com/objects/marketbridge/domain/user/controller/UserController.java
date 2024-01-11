fdb5154095f04acbbeae16d1538a01cea6a4e457
pyoung <pmmh9395@gmail.com>
2024-01-11 09:28:45 +0900

    [ :wrench: | #46 | fix ] User컨트롤러 ApiResponse,@Valid 적용
    회원가입시 컨트롤러에 Valid ,ApiResponse 적용

--- src/main/java/com/objects/marketbridge/domain/user/controller/UserController.java
+++ src/main/java/com/objects/marketbridge/domain/user/controller/UserController.java
@@ -3,8 +3,10 @@ package com.objects.marketbridge.domain.user.controller;
 
 import com.objects.marketbridge.domain.user.dto.CreateUser;
 import com.objects.marketbridge.domain.user.service.UserService;
+import com.objects.marketbridge.global.common.ApiResponse;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,10 +19,9 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-
     @PostMapping("/signup")
-    public ResponseEntity<String> registerUser(@RequestBody CreateUser userDTO) {
+    public ApiResponse<Void> registerUser(@Valid @RequestBody CreateUser userDTO) {
         userService.saveUser(userDTO);
-        return ResponseEntity.ok("User registered successfully");
+        return ApiResponse.of(HttpStatus.CREATED,"completed",null);
     }
 }
