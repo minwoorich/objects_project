b656e57370bf07577ead1e28aaf67126ecd60e29
Jhung Minwoo <minwoorich96@gmail.com>
2024-01-12 13:10:08 +0900

    [ :sparkles: | #27 | feature ] request -> DTO 코드 생성
    - request 가 서비스로 바로 넘어가게 되면 서비스 로직이 화면에 너무 의존하게 돼서 그냥 주문 생성 서비스에서 사용할 dto를 하나 따로 만듦

--- src/main/java/com/objects/marketbridge/domain/order/controller/request/CreateOrderRequest.java
+++ src/main/java/com/objects/marketbridge/domain/order/controller/request/CreateOrderRequest.java
@@ -1,5 +1,7 @@
 package com.objects.marketbridge.domain.order.controller.request;
 
+import com.objects.marketbridge.domain.order.dto.CreateProdOrderDetailDto;
+import com.objects.marketbridge.domain.order.dto.CreateProdOrderDto;
 import com.objects.marketbridge.domain.order.dto.ProductInfoDto;
 import com.objects.marketbridge.domain.order.exception.CustomLogicException;
 import com.objects.marketbridge.domain.order.exception.ErrorCode;
@@ -8,6 +10,8 @@ import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
 
 @Getter
 @NoArgsConstructor
@@ -22,18 +26,23 @@ public class CreateOrderRequest {
     @NotNull
     private Long totalOrderPrice;
 
+    @NotNull
+    private Long addressId;
+
     private String successUrl;
 
     private String failUrl;
 
+
     private List<ProductInfoDto> productInfos = new ArrayList<>();
 
     @Builder(builderClassName = "requestBuilder", toBuilder = true)
-    public CreateOrderRequest(String payMethod, String orderName, Long totalOrderPrice, String successUrl, String failUrl, @Singular("product") List<ProductInfoDto> productInfos) {
+    public CreateOrderRequest(String payMethod, String orderName, Long totalOrderPrice, Long addressId, String successUrl, String failUrl, @Singular("product") List<ProductInfoDto> productInfos) {
         validPaymentAmount();
         this.payMethod = payMethod;
         this.orderName = orderName;
         this.totalOrderPrice = totalOrderPrice;
+        this.addressId = addressId;
         this.successUrl = successUrl;
         this.failUrl = failUrl;
         this.productInfos = productInfos;
@@ -41,7 +50,25 @@ public class CreateOrderRequest {
 
     private void validPaymentAmount() {
         if (totalOrderPrice > 1000000000 || totalOrderPrice < 1000) {
-            throw new CustomLogicException(ErrorCode.INVALID_PAYMENT_AMOUNT);
+            throw new CustomLogicException("금액은 최대 1,000,000,000원 그리고 최저 1,000 원 만 거래 가능합니다", ErrorCode.INVALID_PAYMENT_AMOUNT);
         }
+    }
+
+    public CreateProdOrderDto toProdOrderDto(Long memberId){
+        return CreateProdOrderDto.builder()
+                .memberId(memberId)
+                .addressId(this.addressId)
+                .totalOrderPrice(this.totalOrderPrice)
+                .build();
+    }
+
+    public List<CreateProdOrderDetailDto> toProdOrderDetailDtos() {
+        return productInfos.stream()
+                .map(p -> CreateProdOrderDetailDto.builder()
+                        .productId(p.getProductId())
+                        .usedCouponId(p.getUsedCouponId())
+                        .quantity(p.getQuantity())
+                        .unitOrderPrice(p.getUnitOrderPrice()).build())
+                .collect(Collectors.toList());
     }
 }
