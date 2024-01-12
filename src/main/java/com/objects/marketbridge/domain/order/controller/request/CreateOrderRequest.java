4d75cf2ac179ff7e13826812cc20eecb1c69cd09
Jhung Minwoo <minwoorich96@gmail.com>
2024-01-12 16:35:30 +0900

    [ :sparkles: | #27 | feature ] 클라이언트 요청 받을시 데이터 검증 로직 추가
    - 총 주문 금액이 제대로 맞는지 검증하는 코드 생성
    - 금액이 틀리면 발생시킬 OrderPriceMismatchException 도 생성함

--- src/main/java/com/objects/marketbridge/domain/order/controller/request/CreateOrderRequest.java
+++ src/main/java/com/objects/marketbridge/domain/order/controller/request/CreateOrderRequest.java
@@ -5,6 +5,7 @@ import com.objects.marketbridge.domain.order.dto.CreateProdOrderDto;
 import com.objects.marketbridge.domain.order.dto.ProductInfoDto;
 import com.objects.marketbridge.domain.order.exception.CustomLogicException;
 import com.objects.marketbridge.domain.order.exception.ErrorCode;
+import com.objects.marketbridge.global.error.OrderPriceMismatchException;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
@@ -38,7 +39,8 @@ public class CreateOrderRequest {
 
     @Builder(builderClassName = "requestBuilder", toBuilder = true)
     public CreateOrderRequest(String payMethod, String orderName, Long totalOrderPrice, Long addressId, String successUrl, String failUrl, @Singular("product") List<ProductInfoDto> productInfos) {
-        validPaymentAmount();
+        validPaymentAmount(totalOrderPrice);
+        validTotalOrderPrice(totalOrderPrice, productInfos);
         this.payMethod = payMethod;
         this.orderName = orderName;
         this.totalOrderPrice = totalOrderPrice;
@@ -48,9 +50,19 @@ public class CreateOrderRequest {
         this.productInfos = productInfos;
     }
 
-    private void validPaymentAmount() {
-        if (totalOrderPrice > 1000000000 || totalOrderPrice < 1000) {
+    private void validPaymentAmount(Long totalOrderPrice) {
+        if (totalOrderPrice > 1000000000L || totalOrderPrice < 1000L) {
             throw new CustomLogicException("금액은 최대 1,000,000,000원 그리고 최저 1,000 원 만 거래 가능합니다", ErrorCode.INVALID_PAYMENT_AMOUNT);
+        }
+    }
+
+    private void validTotalOrderPrice(Long totalOrderPrice, List<ProductInfoDto> productInfos) {
+        Long sum = productInfos.stream()
+                .mapToLong(ProductInfoDto::getUnitOrderPrice)
+                .sum();
+
+        if (!sum.equals(totalOrderPrice)) {
+            throw new OrderPriceMismatchException("전달받은 totalOrderPrice : "+totalOrderPrice+", 계산한 totaOrderPrice : "+sum);
         }
     }
 
