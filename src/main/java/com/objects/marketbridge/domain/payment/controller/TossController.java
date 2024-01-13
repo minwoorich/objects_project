da335efbc6cbe360d25b546b5add9ad76cb60827
Jhung Minwoo <minwoorich96@gmail.com>
2024-01-13 21:00:20 +0900

    [ :sparkles: | #27 | feature ] TossController 코드 추가
    - success 로 리다이렉트 될 시
    payment 엔티티 생성 및 DB 저장

--- src/main/java/com/objects/marketbridge/domain/payment/controller/TossController.java
+++ src/main/java/com/objects/marketbridge/domain/payment/controller/TossController.java
@@ -1,18 +1,42 @@
 package com.objects.marketbridge.domain.payment.controller;
 
-import com.objects.marketbridge.domain.payment.controller.response.TossPaymentResponse;
+import com.objects.marketbridge.domain.payment.controller.response.TossPaymentsResponse;
+import com.objects.marketbridge.domain.payment.service.CreatePaymentService;
+import com.objects.marketbridge.domain.payment.service.PaymentService;
+import com.objects.marketbridge.domain.payment.service.TossPaymentService;
 import com.objects.marketbridge.global.common.ApiResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @RestController
+@RequiredArgsConstructor
 public class TossController {
+
+    private final CreatePaymentService createPaymentService;
+    private final TossPaymentService tossPaymentService;
+
     @GetMapping("/payments/toss/success")
-    public ApiResponse<TossPaymentResponse> tossPaymentSuccess(
+    public ApiResponse<TossPaymentsResponse> tossPaymentSuccess(
+            @SessionAttribute Long memberId,
             @RequestParam String paymentKey,
-            @RequestParam Long orderId,
+            @RequestParam(name = "orderId") String orderNo,
+            @RequestParam(name ="amount") Long totalPrice) {
+
+        createPaymentService.create(memberId, paymentKey, orderNo, totalPrice);
+        tossPaymentService.requestPaymentAccept(memberId, paymentKey, orderNo, totalPrice);
+        return null;
+
+    }
+
+    @GetMapping("/payments/toss/fail")
+    public ApiResponse<TossPaymentsResponse> tossPaymentFail(
+            @RequestParam String paymentKey,
+            @RequestParam(name = "orderId") String orderNo,
             @RequestParam Long amount) {
+
 
         return null;
 
