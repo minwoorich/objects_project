dfdfa010cbfecbe69e8ad16506e31b9fc3ecc4c4
Jhung Minwoo <minwoorich96@gmail.com>
2024-01-13 19:32:05 +0900

    [ :sparkles: | #27 | feature ] OrderService 대격변
    - OrderService와 외부API연동서비스의 트랜잭션을 분리함
    
    - 분리하는 과정에서 내부 호출 이슈 발생하게되어 CreateOrderService 를 따로 하나 생성함.
    
    static inner 클래스로 만들어도 되나 그냥 DI 주입 하는 식으로 구현했음

--- src/main/java/com/objects/marketbridge/domain/order/service/OrderService.java
+++ src/main/java/com/objects/marketbridge/domain/order/service/OrderService.java
@@ -1,112 +1,33 @@
 package com.objects.marketbridge.domain.order.service;
 
-import com.objects.marketbridge.address.repository.AddressRepository;
-import com.objects.marketbridge.domain.coupon.repository.CouponRepository;
 import com.objects.marketbridge.domain.member.repository.MemberRepository;
-import com.objects.marketbridge.domain.model.Address;
-import com.objects.marketbridge.domain.model.Coupon;
 import com.objects.marketbridge.domain.model.Member;
-import com.objects.marketbridge.domain.model.Product;
-import com.objects.marketbridge.domain.order.domain.ProdOrder;
-import com.objects.marketbridge.domain.order.domain.ProdOrderDetail;
-import com.objects.marketbridge.domain.order.domain.StatusCodeType;
+import com.objects.marketbridge.domain.order.controller.response.CreateOrderResponse;
 import com.objects.marketbridge.domain.order.dto.CreateProdOrderDetailDto;
 import com.objects.marketbridge.domain.order.dto.CreateProdOrderDto;
-import com.objects.marketbridge.domain.order.service.port.OrderDetailRepository;
-import com.objects.marketbridge.domain.order.service.port.OrderRepository;
-import com.objects.marketbridge.domain.payment.domain.Payment;
-import com.objects.marketbridge.domain.payment.service.PaymentService;
-import com.objects.marketbridge.domain.payment.service.port.PaymentRepository;
-import com.objects.marketbridge.domain.product.repository.ProductRepository;
+import com.objects.marketbridge.domain.payment.config.TossPaymentConfig;
 import com.objects.marketbridge.global.error.EntityNotFoundException;
-import com.objects.marketbridge.global.utils.GroupingHelper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Map;
 
 @Service
-@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class OrderService {
 
-    private final OrderRepository orderRepository;
-    private final OrderDetailRepository orderDetailRepository;
-    private final ProductRepository productRepository;
+    private final CreateOrderService createOrderService;
     private final MemberRepository memberRepository;
-    private final CouponRepository couponRepository;
-    private final AddressRepository addressRepository;
-    private final PaymentService paymentService;
+    private final TossPaymentConfig paymentConfig;
 
-    @Transactional
-    public void create(CreateProdOrderDto prodOrderDto, List<CreateProdOrderDetailDto> prodOrderDetailDtos) {
+    public CreateOrderResponse create(CreateProdOrderDto prodOrderDto, List<CreateProdOrderDetailDto> prodOrderDetailDtos) {
+        createOrderService.orderCreate(prodOrderDto, prodOrderDetailDtos);
+        Member member = memberRepository.findById(prodOrderDto.getMemberId()).orElseThrow(() -> new EntityNotFoundException("엔티티가 존재하지 않습니다"));
 
-        // 1. ProdOrder, ProdOrderDetail 엔티티 생성
-        ProdOrder prodOrder = createProdOrder(prodOrderDto);
-        orderRepository.save(prodOrder);
-        List<ProdOrderDetail> prodOrderDetails = createProdOrderDetails(prodOrderDetailDtos);
+        String email          = member.getEmail();
+        String successUrl     = paymentConfig.getSuccessUrl();
+        String failUrl        = paymentConfig.getFailUrl();
 
-        // 2. 양방향관계이므로 양쪽 다 서로의 값을 집어넣어줘야함
-        prodOrderDetails.forEach(prodOrder::addOrderDetail);
-        prodOrderDetails.forEach(p -> p.setOrder(prodOrder));
-
-        // 3.주문 엔티티 저장
-        orderDetailRepository.saveAll(prodOrderDetails);
-
-        // 4.결제 엔티티 저장
-        paymentService.create(prodOrderDto);
-
-    }
-
-    private ProdOrder createProdOrder(CreateProdOrderDto dto) {
-
-        Member member         = memberRepository.findById(dto.getMemberId()).orElseThrow(IllegalArgumentException::new);
-        Address address       = addressRepository.findById(dto.getAddressId());
-        Long totalOrderPrice  = dto.getTotalOrderPrice();
-        String orderName      = dto.getOrderName();
-        String orderNo        = dto.getOrderNo();
-
-        return ProdOrder.create(member, address,orderName, orderNo, totalOrderPrice);
-    }
-
-    private List<ProdOrderDetail> createProdOrderDetails(List<CreateProdOrderDetailDto> dtos) {
-
-        // request 에서 Product 를 추출하여 Map 으로 그룹핑
-        Map<Long, Product> productMap = GroupingHelper.groupingByKey(getAllProducts(dtos), Product::getId);
-
-        // request 에서 Coupon 을 추출하여 Map 으로 그룹핑
-        Map<Long, Coupon> couponMap = GroupingHelper.groupingByKey(getAllCoupons(dtos), Coupon::getId);
-
-        return dtos.stream().map(d ->
-                ProdOrderDetail.create(
-                        productMap.get(d.getProductId()),
-                        couponMap.get(d.getUsedCouponId()),
-                        d.getQuantity(),
-                        d.getUnitOrderPrice(),
-                        StatusCodeType.ORDER_INIT.getCode()
-                )
-        ).toList();
-    }
-
-    private List<Coupon> getAllCoupons(List<CreateProdOrderDetailDto> dtos) {
-        return couponRepository.findAllByIds(extractCouponIds(dtos));
-    }
-
-    private List<Product> getAllProducts(List<CreateProdOrderDetailDto> dtos) {
-        return productRepository.findAllById(extractProductIds(dtos));
-    }
-
-    private static List<Long> extractCouponIds(List<CreateProdOrderDetailDto> dtos) {
-        return dtos.stream()
-                .map(CreateProdOrderDetailDto::getUsedCouponId)
-                .toList();
-    }
-
-    private static List<Long> extractProductIds(List<CreateProdOrderDetailDto> dtos) {
-        return dtos.stream()
-                .map(CreateProdOrderDetailDto::getProductId)
-                .toList();
+        return CreateOrderResponse.from(prodOrderDto, email, successUrl, failUrl);
     }
 }
