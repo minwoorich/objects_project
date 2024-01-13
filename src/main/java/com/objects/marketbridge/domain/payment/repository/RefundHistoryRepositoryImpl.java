1657cab400556413dab48b91dc85fb1527604965
Jhung Minwoo <minwoorich96@gmail.com>
2024-01-13 19:27:19 +0900

    [ :truck: |  | chore ] 에러나는 코드 에러안나게 수정

--- src/main/java/com/objects/marketbridge/domain/payment/repository/RefundHistoryRepositoryImpl.java
+++ src/main/java/com/objects/marketbridge/domain/payment/repository/RefundHistoryRepositoryImpl.java
@@ -3,7 +3,10 @@ package com.objects.marketbridge.domain.payment.repository;
 import com.objects.marketbridge.domain.payment.domain.RefundHistory;
 import org.springframework.stereotype.Repository;
 
-public interface RefundHistoryRepository {
-    // TODO Spring Data JPA 로 구현해야함!
-    void save(RefundHistory refundHistory);
+@Repository
+public class RefundHistoryRepositoryImpl implements RefundHistoryRepository {
+    @Override
+    public void save(RefundHistory refundHistory) {
+
+    }
 }
