e9c5d58a36f61e5eb1be3aae38e2572285eaacdc
Jhung Minwoo <minwoorich96@gmail.com>
2024-01-13 21:02:12 +0900

    [ :sparkles: | #27 | feature ] CreatePaymentService 생성
    - 기존 PaymentService 가 하던 역할(Payment생성) 을 CreatePaymentService 가 맡게됨

--- src/main/java/com/objects/marketbridge/domain/payment/service/PaymentService.java
+++ src/main/java/com/objects/marketbridge/domain/payment/service/PaymentService.java
@@ -1,11 +1,5 @@
 package com.objects.marketbridge.domain.payment.service;
 
-import com.objects.marketbridge.domain.member.repository.MemberRepository;
-import com.objects.marketbridge.domain.model.Member;
-import com.objects.marketbridge.domain.order.dto.CreateProdOrderDto;
-import com.objects.marketbridge.domain.payment.domain.Payment;
-import com.objects.marketbridge.domain.payment.service.port.PaymentRepository;
-import com.objects.marketbridge.global.error.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,22 +8,5 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class PaymentService {
-    private final PaymentRepository paymentRepository;
-    private final MemberRepository memberRepository;
 
-    @Transactional
-    public void create(CreateProdOrderDto prodOrderDto) {
-
-        Member member = memberRepository.findById(prodOrderDto.getMemberId()).orElseThrow(() -> new EntityNotFoundException("엔티티가 존재하지 않습니다"));
-
-        String memberName     = member.getName();
-        String phoneNo        = member.getPhoneNo();
-        String orderName      = prodOrderDto.getOrderName();
-        Long totalOrderPrice  = prodOrderDto.getTotalOrderPrice();
-        String orderNo        = prodOrderDto.getOrderNo();
-        String paymentMethod  = prodOrderDto.getPaymentMethod();
-
-        Payment payment = Payment.create(memberName, orderName, totalOrderPrice, orderNo, paymentMethod, phoneNo);
-        paymentRepository.save(payment);
-    }
 }
