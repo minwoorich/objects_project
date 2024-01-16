52ff289ebfdc0cc87f637f882c6155b0cfffee18
pyoung <pmmh9395@gmail.com>
2024-01-16 14:10:33 +0900

    [ :wrench: | #80 | fix ] FindPointDto 불필요 id 제거
    FindPointDto 불필요 id 제거

--- src/main/java/com/objects/marketbridge/domain/member/dto/FindPointDto.java
+++ src/main/java/com/objects/marketbridge/domain/member/dto/FindPointDto.java
@@ -11,7 +11,6 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class FindPointDto extends BaseEntity {
 
-    private Long pointId;
     private Long balance;
     private Long inPoint;
     private Long outPoint;
