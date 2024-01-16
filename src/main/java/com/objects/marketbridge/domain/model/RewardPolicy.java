ab8974b1f6159fa8d907288cff120608c08efbd3
Jhung Minwoo <minwoorich96@gmail.com>
2024-01-16 22:00:48 +0900

    [ :wrench: | #27 | fix ]  reward 관련 수정사항
    - RewardPolicy <- 포인트적립 정책을 저장하는 테이블
    - RewardType <- 포인트 적립을 정의해놓은 enum클래스
    
    - ProdOrderDetail : saved_point(number) -> reward_type(str)
    어떤 사유로 포인트가 적립되었는지 정확히 기록하기 위해
    
    - ProdOrderDetail : used_coupon_price 제거 (어차피 couponId 있으니깐)

--- src/main/java/com/objects/marketbridge/domain/model/RewardPolicy.java
+++ src/main/java/com/objects/marketbridge/domain/model/RewardPolicy.java
@@ -9,17 +9,19 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Reward extends BaseEntity{
+public class RewardPolicy extends BaseEntity{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "reward_id")
     private Long id;
 
-    private Integer rate;
+    private String type;
+    private Long rate;
 
     @Builder
-    private Reward(Integer rate) {
+    public RewardPolicy(String type, Long rate) {
+        this.type = type;
         this.rate = rate;
     }
 }
--- src/main/resources/schema-truncate.sql
+++ src/main/resources/schema-truncate.sql
@@ -4,8 +4,6 @@ truncate table admin;
 
 truncate table board;
 
-truncate table cancel_payment;
-
 truncate table cart;
 
 truncate table category;
@@ -56,7 +54,7 @@ truncate table review_survey;
 
 truncate table review_survey_category;
 
-truncate table reward;
+truncate table reward_policy;
 
 truncate table seller;
 
