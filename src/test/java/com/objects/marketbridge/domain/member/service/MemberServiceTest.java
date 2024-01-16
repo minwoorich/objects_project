b9e5a27d9809c7598e23a9c973017b32f20a413b
pyoung <pmmh9395@gmail.com>
2024-01-16 14:00:11 +0900

    [ :sparkles: | #80 | feature ] MemberPoint 조회 TEST
    MemberPoint 테스트코드 작성
    MemberServiceTest 리팩토링

--- src/test/java/com/objects/marketbridge/domain/member/service/MemberServiceTest.java
+++ src/test/java/com/objects/marketbridge/domain/member/service/MemberServiceTest.java
@@ -1,10 +1,15 @@
 package com.objects.marketbridge.domain.member.service;
 
 
+import com.objects.marketbridge.domain.member.dto.FindPointDto;
 import com.objects.marketbridge.domain.model.Member;
 import com.objects.marketbridge.domain.member.repository.MemberRepository;
 import com.objects.marketbridge.domain.model.Membership;
+import com.objects.marketbridge.domain.model.Point;
+import com.objects.marketbridge.domain.point.repository.PointRepository;
+import com.objects.marketbridge.global.error.EntityNotFoundException;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -27,13 +32,21 @@ class MemberServiceTest {
     @Autowired
     MemberRepository memberRepository;
 
-    private Member existingMember;
+    @Autowired
+    PointRepository pointRepository;
+
+    Member originMember;
 
     @BeforeEach
     public void createMemberOrigin() {
         // 생성된 사용자 객체를 저장할 변수
-        existingMember = Member.builder().email("test1234@gmail.com").membership(Membership.BASIC).build();
-        memberRepository.save(existingMember);
+        originMember=memberRepository.save(createMember("test1234@gmail.com"));
+    }
+
+    @AfterEach
+    public void rollback(){
+        memberRepository.deleteAll();
+        pointRepository.deleteAllInBatch();
     }
 
     @Test
@@ -69,10 +82,41 @@ class MemberServiceTest {
         //given
         Membership memberShipData = Membership.WOW;
         //when
-        memberService.changeMemberShip(existingMember.getId());
+        memberService.changeMemberShip(originMember.getId());
         //then
-        assertThat(existingMember.getMembership()).isEqualTo(memberShipData);
+        assertThat(originMember.getMembership()).isEqualTo(memberShipData);
     }
 
+
+    @Test
+    @DisplayName("포인트 조회 API")
+    public void testFindPointById(){
+            //given
+        Member member = memberRepository.findByEmail("test1234@gmail.com").orElseThrow(() -> new EntityNotFoundException("엔티티가 존재하지 않습니다"));
+        Point point = createPoint(member, 4500L);
+        pointRepository.save(point);
+
+            //when
+        FindPointDto findMember = memberService.findPointById(member.getId());
+
+
+            //then
+        assertThat(findMember.getBalance()).isEqualTo(4500L);
+
+    }
+
+    private Member createMember(String email) {
+        return Member.builder()
+                .email(email)
+                .membership(Membership.BASIC)
+                .build();
+    }
+
+    private Point createPoint(Member member, Long balance) {
+        Point point = Point.builder().balance(balance).member(member).build();
+        point.setMember(member);
+
+        return point;
+    }
 
 }
