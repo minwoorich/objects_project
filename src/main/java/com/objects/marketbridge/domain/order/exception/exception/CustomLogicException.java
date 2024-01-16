b340f425e5b1bcb8ca4031910f781293afdd9b46
Jhung Minwoo <minwoorich96@gmail.com>
2024-01-16 17:11:52 +0900

    [ :wrench: | #27 | fix ] CustomLogicException 수정
    - 생성자 파라미터에 Enum 타입 말고 String 타입으로도 code 넣을 수 있게 변경

--- src/main/java/com/objects/marketbridge/domain/order/exception/exception/CustomLogicException.java
+++ src/main/java/com/objects/marketbridge/domain/order/exception/exception/CustomLogicException.java
@@ -6,10 +6,15 @@ import lombok.NoArgsConstructor;
 public class CustomLogicException extends RuntimeException {
 
     private ErrorCode errorCode;
+    private String errorCodeStr;
     private String message;
 
     public CustomLogicException(String message, ErrorCode errorCode) {
         super(message);
         this.errorCode = errorCode;
+    }
+    public CustomLogicException(String message, String errorCodeStr) {
+        super(message);
+        this.errorCodeStr = errorCodeStr;
     }
 }
