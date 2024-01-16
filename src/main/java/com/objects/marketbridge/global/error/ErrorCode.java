296507926cb625a6e34894f5ca5b5c6a175a79e4
Jhung Minwoo <minwoorich96@gmail.com>
2024-01-16 19:55:02 +0900

    [ :sparkles: | #27 | feature ]  MISMATCHED_PAYMENT_DATA 에러 코드 타입 추가

--- src/main/java/com/objects/marketbridge/global/error/ErrorCode.java
+++ src/main/java/com/objects/marketbridge/global/error/ErrorCode.java
@@ -1,4 +1,4 @@
-package com.objects.marketbridge.domain.order.exception.exception;
+package com.objects.marketbridge.global.error;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 public enum ErrorCode {
 
     INVALID_PAYMENT_AMOUNT("유효하지 않은 액수"),
+    MISMATCHED_PAYMENT_DATA("결제정보 검증에 실패하였습니다"),
     SHIPPING_ADDRESS_NOT_REGISTERED("등록된 배송지 정보가 없습니다.");
 
     private final String message;
