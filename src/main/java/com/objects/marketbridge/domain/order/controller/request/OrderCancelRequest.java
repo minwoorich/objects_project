fe0845958f120273339a6bedc161a0c311d16125
sunmok <tjsahr9191@naver.com>
2024-01-18 02:00:19 +0900

    [ :sparkles: | #34 | feature ] 주문 취소 Request 객체

--- src/main/java/com/objects/marketbridge/domain/order/controller/request/OrderCancelRequest.java
+++ src/main/java/com/objects/marketbridge/domain/order/controller/request/OrderCancelRequest.java
@@ -1,4 +1,31 @@
 package com.objects.marketbridge.domain.order.controller.request;
 
+import com.objects.marketbridge.domain.order.dto.OrderCancelServiceDto;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class OrderCancelRequest {
+
+    @NotNull
+    private Long orderId;
+
+    @NotNull
+    private String reason;
+
+    @Builder
+    public OrderCancelRequest(Long orderId, String reason) {
+        this.orderId = orderId;
+        this.reason = reason;
+    }
+
+    public OrderCancelServiceDto toServiceRequest() {
+        return OrderCancelServiceDto.builder()
+                .orderId(orderId)
+                .reason(reason)
+                .build();
+    }
 }
