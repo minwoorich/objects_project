f6b2708d90962d2b4863b240b2c32b5de7a89468
sunmok <tjsahr9191@naver.com>
2024-01-19 10:56:20 +0900

    [ :wrench: | #34 | fix ] 토스 페이먼츠 환불

--- src/main/java/com/objects/marketbridge/domain/order/service/OrderCancelService.java
+++ src/main/java/com/objects/marketbridge/domain/order/service/OrderCancelService.java
@@ -1,11 +1,16 @@
 package com.objects.marketbridge.domain.order.service;
 
+import com.objects.marketbridge.domain.member.repository.MemberRepository;
 import com.objects.marketbridge.domain.order.controller.response.OrderCancelResponse;
 import com.objects.marketbridge.domain.order.dto.OrderCancelServiceDto;
 import com.objects.marketbridge.domain.order.entity.ProdOrder;
 import com.objects.marketbridge.domain.order.service.port.OrderRepository;
+import com.objects.marketbridge.domain.payment.domain.Payment;
 import com.objects.marketbridge.domain.payment.dto.RefundDto;
 import com.objects.marketbridge.domain.payment.service.RefundService;
+import com.objects.marketbridge.domain.payment.service.port.PaymentRepository;
+import com.objects.marketbridge.global.error.CustomLogicException;
+import com.objects.marketbridge.global.error.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,31 +24,36 @@ import static com.objects.marketbridge.domain.order.entity.StatusCodeType.*;
 public class OrderCancelService {
 
     private final OrderRepository orderRepository;
+    private final PaymentRepository paymentRepository;
 
     private final RefundService refundService;
-    
+
     // TODO 트랜잭션 위치 고려해야함
     @Transactional
     public OrderCancelResponse orderCancel(OrderCancelServiceDto orderCancelServiceDto, LocalDateTime cancelDateTime) {
         InnerService innerService = new InnerService();
-        Long orderId = orderCancelServiceDto.getOrderId();
-        String reason = orderCancelServiceDto.getReason();
 
-        ProdOrder order = innerService.cancel(orderId, reason, cancelDateTime);
+        ProdOrder order = innerService.cancel(
+                orderCancelServiceDto.getOrderId(),
+                orderCancelServiceDto.getCancelReason(),
+                cancelDateTime
+        );
 
-        // TODO 결제 취소(환불)
-//        RefundDto refund = refundService.refund("계좌번호", 10000L);
+        Payment payment = validPayment(orderCancelServiceDto.getOrderId());
 
-        return OrderCancelResponse.of(order, RefundDto.builder()
-                .totalRefundAmount(10000L)
-                .refundMethod("card")
-                .refundProcessedAt(LocalDateTime.of(2024, 1, 17, 10, 20))
-                .build());
+        RefundDto refundDto = refundService.refund(
+                payment,
+                orderCancelServiceDto.getCancelReason(),
+                order.getRealPrice()
+        );
+
+        return OrderCancelResponse.of(order, refundDto);
     }
 
 
     // TODO 객체로 따로 빼야함(임시로 사용)
     class InnerService {
+
         public ProdOrder cancel(Long orderId, String reason, LocalDateTime cancelDateTime) {
             ProdOrder prodOrder = orderRepository.findProdOrderWithDetailsAndProduct(orderId)
                     .orElseThrow(() -> new IllegalArgumentException("해당하는 주문이 없습니다."));
@@ -55,6 +65,14 @@ public class OrderCancelService {
             return prodOrder;
         }
 
+    }
+
+    private Payment validPayment(Long orderId) {
+        Payment payment = paymentRepository.findByOrderId(orderId);
+        if (payment == null) {
+            throw new CustomLogicException(ErrorCode.PAYMENT_NOT_FOUND.getMessage());
+        }
+        return payment;
     }
 
 }
