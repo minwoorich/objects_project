f1e5be3c023ef4e399e7940c5810212b67c7b7c1
Jhung Minwoo <minwoorich96@gmail.com>
2024-01-26 13:55:11 +0900

    [ :recycle: | #162 | refactor ]  KakaoPayReadyService 시연 성공 (테스트 코드 X)

--- src/main/java/com/objects/marketbridge/domain/order/service/KakaoPayReadyService.java
+++ src/main/java/com/objects/marketbridge/domain/order/service/KakaoPayReadyService.java
@@ -5,10 +5,18 @@ import com.objects.marketbridge.domain.order.dto.KakaoPayReadyRequest;
 import com.objects.marketbridge.domain.payment.config.KakaoPayConfig;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.FormHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestClient;
 
+import java.util.List;
+
 import static com.objects.marketbridge.domain.payment.config.KakaoPayConfig.KAKAO_BASE_URL;
+import static org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 
 @Service
 @RequiredArgsConstructor
@@ -16,15 +24,23 @@ public class KakaoPayReadyService {
 
     private final KakaoPayConfig kakaoPayConfig;
     public KakaoPayReadyResponse execute(KakaoPayReadyRequest request) {
+
+        MultiValueMap<String, String> requestMap = request.toMultiValueMap();
+
         RestClient restClient = RestClient.builder()
                 .baseUrl(KAKAO_BASE_URL)
-                .defaultHeader("Authorization", "KaKaoAK "+ kakaoPayConfig.getAdminKey())
+                .messageConverters((converters) ->
+                    converters.add(new FormHttpMessageConverter()))
+                .defaultHeaders((httpHeaders -> {
+                    httpHeaders.add("Authorization", "KakaoAK "+kakaoPayConfig.getAdminKey());
+                    httpHeaders.add("Accept", APPLICATION_JSON.toString());
+                    httpHeaders.add("Content-Type", APPLICATION_FORM_URLENCODED.toString()+";charset=UTF-8");
+                }))
                 .build();
 
         return restClient.post()
                 .uri("/ready")
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .body(request)
+                .body(requestMap)
                 .retrieve()
                 .body(KakaoPayReadyResponse.class);
     }
