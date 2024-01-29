600b480e4c30ca1c61f438192ce50db3d97eae8a
Jhung Minwoo <minwoorich96@gmail.com>
2024-01-29 14:41:11 +0900

    [ :sparkles: | #186 | feature ] OrderDetail 에 쿠폰금액 가져오는 메서드 생성

--- src/main/java/com/objects/marketbridge/order/domain/OrderDetail.java
+++ src/main/java/com/objects/marketbridge/order/domain/OrderDetail.java
@@ -72,6 +72,10 @@ public class OrderDetail extends BaseEntity {
         this.statusCode = statusCode;
     }
 
+    public Long getCouponPrice() {
+        return coupon != null ? coupon.getPrice() : 0L;
+    }
+
     public static OrderDetail create(String tid, Order order, Product product, String orderNo, Coupon coupon, Long quantity, Long price, String statusCode) {
 
         return OrderDetail.builder()
