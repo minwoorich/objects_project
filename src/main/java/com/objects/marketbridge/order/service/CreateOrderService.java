aef383773124f2da8ab52ec97135dbe76afaf31d
Jhung Minwoo <minwoorich96@gmail.com>
2024-01-29 14:38:35 +0900

    [ :recycle: | #186 | refactor ] CreateOrderService 리팩토링
    - 쿠폰 사용 금액만 계산하는 로직에서 추후 확장성을 위해 전체 할인 금액을 계산하는 메서드로 변경.

--- src/main/java/com/objects/marketbridge/order/service/CreateOrderService.java
+++ src/main/java/com/objects/marketbridge/order/service/CreateOrderService.java
@@ -1,17 +1,13 @@
 package com.objects.marketbridge.order.service;
 
 import com.objects.marketbridge.common.domain.*;
+import com.objects.marketbridge.order.domain.*;
 import com.objects.marketbridge.order.service.port.AddressRepository;
 import com.objects.marketbridge.order.service.port.OrderDetailCommendRepository;
 import com.objects.marketbridge.product.infra.CouponRepository;
 import com.objects.marketbridge.product.infra.MemberCouponRepository;
 import com.objects.marketbridge.member.service.port.MemberRepository;
 import com.objects.marketbridge.order.service.dto.CreateOrderDto;
-import com.objects.marketbridge.order.domain.Order;
-import com.objects.marketbridge.order.domain.OrderDetail;
-import com.objects.marketbridge.order.domain.ProductValue;
-import com.objects.marketbridge.order.domain.StatusCodeType;
-import com.objects.marketbridge.order.service.port.OrderDetailQueryRepository;
 import com.objects.marketbridge.order.service.port.OrderCommendRepository;
 import com.objects.marketbridge.product.infra.ProductRepository;
 import lombok.RequiredArgsConstructor;
@@ -30,7 +26,6 @@ import java.util.stream.Collectors;
 @Slf4j
 public class CreateOrderService {
 
-    private final OrderDetailQueryRepository orderDetailQueryRepository;
     private final OrderDetailCommendRepository orderDetailCommendRepository;
     private final OrderCommendRepository orderCommendRepository;
     private final ProductRepository productRepository;
@@ -40,6 +35,7 @@ public class CreateOrderService {
     private final MemberCouponRepository memberCouponRepository;
     private final CouponUsageService couponUsageService;
     private final ProductStockService productStockService;
+    private final CalcTotalDiscountService calcTotalDiscountService;
 
     @Transactional
     public void create(CreateOrderDto createOrderDto) {
@@ -50,10 +46,11 @@ public class CreateOrderService {
         // 2. OrderDetail 생성 (연관관계 매핑 여기서 해결)
         List<OrderDetail> orderDetails = orderDetailCommendRepository.saveAll(createOrderDetails(createOrderDto.getProductValues(), order));
 
-        // 3. Order 에 최종쿠폰사용 금액 집어넣기
-        order.setTotalUsedCouponPrice(getTotalCouponPrice(orderDetails));
+        // 3. 총 할인 금액(totalDiscount) 저장 및 실 결제 금액(realPrice) 저장
+        order.calcTotalDiscount(calcTotalDiscountService);
 
         // 4. MemberCoupon 의 isUsed 변경
+        order.returnCoupon();
         List<MemberCoupon> memberCoupons = getMemberCoupons(orderDetails, createOrderDto.getMemberId());
         couponUsageService.applyCouponUsage(memberCoupons, true, LocalDateTime.now());
 
@@ -86,10 +83,9 @@ public class CreateOrderService {
         String orderName = createOrderDto.getOrderName();
         String orderNo = createOrderDto.getOrderNo();
         Long totalOrderPrice = createOrderDto.getTotalOrderPrice();
-        Long realOrderPrice = createOrderDto.getRealOrderPrice();
         String tid = createOrderDto.getTid();
 
-        return Order.create(member, address, orderName, orderNo, totalOrderPrice, realOrderPrice, tid);
+        return Order.create(member, address, orderName, orderNo, totalOrderPrice, tid);
     }
 
     private List<OrderDetail> createOrderDetails(List<ProductValue> productValues, Order order) {
