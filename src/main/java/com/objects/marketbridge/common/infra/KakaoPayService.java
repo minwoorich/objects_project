a00920fb1e1e3c972e7f57b6fda9b2b9ea7a2eac
sunmok <tjsahr9191@naver.com>
2024-01-30 23:47:08 +0900

    [ :sparkles: | #180 | feature ] kakaoService 취소 메서드 작성

--- src/main/java/com/objects/marketbridge/common/infra/KakaoPayService.java
+++ src/main/java/com/objects/marketbridge/common/infra/KakaoPayService.java
@@ -4,11 +4,12 @@ import com.objects.marketbridge.common.config.KakaoPayConfig;
 import com.objects.marketbridge.common.dto.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.converter.FormHttpMessageConverter;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestClient;
-import org.springframework.web.util.UriBuilder;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import static com.objects.marketbridge.common.config.KakaoPayConfig.*;
@@ -80,6 +81,22 @@ public class KakaoPayService {
     }
 
     // 취소
+    public KaKaoCancelResponse cancel(String tid, Integer cancelAmount) {
+
+        MultiValueMap<String, String> requestMap = new LinkedMultiValueMap<>();
+        requestMap.add("cid", ONE_TIME_CID);
+        requestMap.add("tid", tid);
+        requestMap.add("cancel_amount", String.valueOf(cancelAmount));
+        requestMap.add("cancel_tax_free_amount", "0");
+
+        RestClient restClient = setup();
+
+        return restClient.post()
+                .uri(CANCEL_END_POINT)
+                .body(requestMap)
+                .retrieve()
+                .body(KaKaoCancelResponse.class);
+    }
 
     // 정기구독 비활성화
 
