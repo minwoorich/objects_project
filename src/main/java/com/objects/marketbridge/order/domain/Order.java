2e01d821ce24b62c85455ee0b170b7358322cf46
Jhung Minwoo <minwoorich96@gmail.com>
2024-02-02 12:14:12 +0900

    [ :wrench: | #216 | fix] 에러 수정

--- src/main/java/com/objects/marketbridge/order/domain/Order.java
+++ src/main/java/com/objects/marketbridge/order/domain/Order.java
@@ -121,6 +121,7 @@ public class Order extends BaseEntity {
     // 판매자별 총 주문 금액
     public Map<Long, Long> totalAmountGroupedBySellerId() {
         return orderDetails.stream()
+                .filter(o -> o.getSellerId() != null)
                 .collect(Collectors.groupingBy(OrderDetail::getSellerId,
                         Collectors.summingLong(OrderDetail::totalAmount)));
     }
@@ -128,6 +129,7 @@ public class Order extends BaseEntity {
     // 판매자 별 주문리스트
     public Map<Long, List<OrderDetail>> orderDetailsGroupedBySellerId() {
         return orderDetails.stream()
+                .filter(o -> o.getSellerId() != null)
                 .collect(Collectors.groupingBy(OrderDetail::getSellerId));
     }
 
