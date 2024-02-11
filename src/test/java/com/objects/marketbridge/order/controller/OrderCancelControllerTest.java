40513ee39403cb4211fa8c4c9479123555ec39a3
sunmok <tjsahr9191@naver.com>
2024-02-11 17:13:31 +0900

    [ :white_check_mark: | #180 | test ] 취소 컨트롤러 테스트

--- src/test/java/com/objects/marketbridge/order/controller/OrderCancelControllerTest.java
+++ src/test/java/com/objects/marketbridge/order/controller/OrderCancelControllerTest.java
@@ -6,9 +6,12 @@ import com.objects.marketbridge.member.domain.Coupon;
 import com.objects.marketbridge.member.domain.Member;
 import com.objects.marketbridge.member.domain.MemberCoupon;
 import com.objects.marketbridge.member.domain.MembershipType;
+import com.objects.marketbridge.order.controller.dto.ConfirmCancelHttp;
+import com.objects.marketbridge.order.controller.dto.ConfirmReturnHttp;
 import com.objects.marketbridge.order.controller.dto.GetCancelDetailHttp;
 import com.objects.marketbridge.order.controller.dto.RequestCancelHttp;
 import com.objects.marketbridge.order.domain.MemberShipPrice;
+import com.objects.marketbridge.order.domain.Order;
 import com.objects.marketbridge.order.domain.OrderDetail;
 import com.objects.marketbridge.order.mock.BaseFakeOrderDetailRepository;
 import com.objects.marketbridge.order.mock.BaseFakeOrderRepository;
@@ -22,7 +25,7 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
 
-import static com.objects.marketbridge.common.exception.exceptions.ErrorCode.NON_CANCELLABLE_PRODUCT;
+import static com.objects.marketbridge.common.exception.exceptions.ErrorCode.*;
 import static com.objects.marketbridge.order.domain.MemberShipPrice.WOW;
 import static com.objects.marketbridge.order.domain.StatusCodeType.*;
 import static com.objects.marketbridge.order.domain.StatusCodeType.RELEASE_PENDING;
@@ -35,9 +38,11 @@ import static org.springframework.http.HttpStatus.OK;
 class OrderCancelControllerTest {
 
     private LocalDateTime orderDate = LocalDateTime.of(2024, 2, 9, 3, 9);
+    private LocalDateTime now = LocalDateTime.of(2024, 2, 9, 4, 47);
     private TestContainer testContainer = TestContainer.builder()
             .dateTimeHolder(
                     TestDateTimeHolder.builder()
+                            .now(now)
                             .createTime(orderDate)
                             .build()
             )
@@ -49,6 +54,362 @@ class OrderCancelControllerTest {
         BaseFakeOrderRepository.getInstance().clear();
         testContainer.memberRepository.deleteAllInBatch();
         testContainer.productRepository.deleteAllInBatch();
+    }
+
+    @Test
+    @DisplayName("저장된 주문상세가 없으면 오류가 발생한다.")
+    public void confirmCancel_NoOrderDetail_ERROR() {
+        // given
+        ConfirmCancelHttp.Request request = ConfirmCancelHttp.Request.builder()
+                .orderDetailId(1L)
+                .build();
+
+        // when // then
+        assertThatThrownBy(() -> testContainer.orderCancelController.confirmCancel(request))
+                .isInstanceOf(EntityNotFoundException.class)
+                .hasMessage("엔티티가 존재하지 않습니다");
+    }
+
+    @Test
+    @DisplayName("배송완료된 상품이면 에러를 발생시킨다.")
+    public void confirmCancel_NO_DELIVERY_COMPLETED_ERROR() {
+        // given
+        ConfirmCancelHttp.Request request = ConfirmCancelHttp.Request.builder()
+                .orderDetailId(1L)
+                .numberOfCancellation(2L)
+                .reason("단순변심")
+                .build();
+
+        OrderDetail orderDetail = OrderDetail.builder()
+                .statusCode(DELIVERY_COMPLETED.getCode())
+                .build();
+
+        testContainer.orderDetailCommendRepository.save(orderDetail);
+
+        // when // then
+        assertThatThrownBy(() -> testContainer.orderCancelController.confirmCancel(request))
+                .isInstanceOf(CustomLogicException.class)
+                .hasMessage(NON_CANCELLABLE_PRODUCT.getMessage())
+                .satisfies(exception -> {
+                    CustomLogicException customLogicException = (CustomLogicException) exception;
+                    assertThat(customLogicException.getErrorCode()).isEqualTo(NON_CANCELLABLE_PRODUCT);
+                    assertThat(customLogicException.getHttpStatus()).isEqualTo(BAD_REQUEST);
+                    assertThat(customLogicException.getTimestamp()).isEqualTo(now);
+                });
+    }
+
+    @Test
+    @DisplayName("취소 수량이 기존 수량보다 많으면 에러가 발생한다.")
+    public void confirmCancel_QUANTITY_EXCEEDED_ERROR() {
+        // given
+        ConfirmCancelHttp.Request request = ConfirmCancelHttp.Request.builder()
+                .orderDetailId(1L)
+                .numberOfCancellation(2L)
+                .reason("단순변심")
+                .build();
+
+        OrderDetail orderDetail = OrderDetail.builder()
+                .quantity(1L)
+                .reducedQuantity(0L)
+                .statusCode(ORDER_RECEIVED.getCode())
+                .build();
+
+        testContainer.orderDetailCommendRepository.save(orderDetail);
+
+        // when // then
+        assertThatThrownBy(() -> testContainer.orderCancelController.confirmCancel(request))
+                .isInstanceOf(CustomLogicException.class)
+                .hasMessage(QUANTITY_EXCEEDED.getMessage())
+                .satisfies(exception -> {
+                    CustomLogicException customLogicException = (CustomLogicException) exception;
+                    assertThat(customLogicException.getErrorCode()).isEqualTo(QUANTITY_EXCEEDED);
+                    assertThat(customLogicException.getHttpStatus()).isEqualTo(BAD_REQUEST);
+                    assertThat(customLogicException.getTimestamp()).isEqualTo(now);
+                });
+    }
+
+    @Test
+    @DisplayName("부분 취소면 줄어든 수량, 상품재고가 변경된다.(NoCoupon)")
+    public void confirmCancel_Partial_NoCoupon() {
+        // given
+        ConfirmCancelHttp.Request request = ConfirmCancelHttp.Request.builder()
+                .orderDetailId(1L)
+                .numberOfCancellation(2L)
+                .reason("단순변심")
+                .build();
+
+        Order order = Order.builder()
+                .build();
+
+        Product product = Product.builder()
+                .name("빵빵이키링")
+                .productNo("1")
+                .stock(5L)
+                .price(1000L)
+                .build();
+
+        LocalDateTime cancelledAt = LocalDateTime.of(2024, 2, 11, 3, 23);
+        OrderDetail orderDetail = OrderDetail.builder()
+                .order(order)
+                .product(product)
+                .quantity(3L)
+                .orderNo("1")
+                .price(1000L)
+                .reducedQuantity(0L)
+                .statusCode(ORDER_RECEIVED.getCode())
+                .cancelledAt(cancelledAt)
+                .build();
+
+        testContainer.orderCommendRepository.save(order);
+        testContainer.orderDetailCommendRepository.save(orderDetail);
+        testContainer.productRepository.save(product);
+
+        // when
+        ApiResponse<ConfirmCancelHttp.Response> result = testContainer.orderCancelController.confirmCancel(request);
+
+        // then
+        assertThat(orderDetail.getStatusCode()).isEqualTo(ORDER_RECEIVED.getCode());
+        assertThat(orderDetail.getReason()).isNull();
+        assertThat(product.getStock()).isEqualTo(7L);
+
+        assertThat(result.getCode()).isEqualTo(OK.value());
+        assertThat(result.getStatus()).isEqualTo(OK);
+        assertThat(result.getMessage()).isEqualTo(OK.name());
+
+        assertThat(result.getData().getOrderId()).isEqualTo(1L);
+        assertThat(result.getData().getOrderNo()).isEqualTo("1");
+        assertThat(result.getData().getTotalPrice()).isEqualTo(2000L);
+        assertThat(result.getData().getCancellationDate()).isEqualTo(cancelledAt);
+
+        assertThat(result.getData().getCancelledItem().getProductId()).isEqualTo(1L);
+        assertThat(result.getData().getCancelledItem().getProductNo()).isEqualTo("1");
+        assertThat(result.getData().getCancelledItem().getName()).isEqualTo("빵빵이키링");
+        assertThat(result.getData().getCancelledItem().getPrice()).isEqualTo(1000L);
+        assertThat(result.getData().getCancelledItem().getQuantity()).isEqualTo(2L);
+
+        assertThat(result.getData().getRefundInfo().getTotalRefundAmount()).isEqualTo(2000L);
+        assertThat(result.getData().getRefundInfo().getRefundMethod()).isEqualTo("카드");
+        assertThat(result.getData().getRefundInfo().getRefundProcessedAt()).isEqualTo(this.now);
+    }
+
+    @Test
+    @DisplayName("모두 취소면 상품재고, 줄어든 수량, 상태코드, 이유가 변경된다.(NoCoupon)")
+    public void confirmCancel_All_NoCoupon() {
+        // given
+        ConfirmCancelHttp.Request request = ConfirmCancelHttp.Request.builder()
+                .orderDetailId(1L)
+                .numberOfCancellation(3L)
+                .reason("단순변심")
+                .build();
+
+        Order order = Order.builder()
+                .build();
+
+        Product product = Product.builder()
+                .name("빵빵이키링")
+                .productNo("1")
+                .stock(5L)
+                .price(1000L)
+                .build();
+
+        LocalDateTime cancelledAt = LocalDateTime.of(2024, 2, 11, 3, 23);
+        OrderDetail orderDetail = OrderDetail.builder()
+                .order(order)
+                .product(product)
+                .quantity(3L)
+                .orderNo("1")
+                .price(1000L)
+                .reducedQuantity(0L)
+                .statusCode(ORDER_RECEIVED.getCode())
+                .cancelledAt(cancelledAt)
+                .build();
+
+        testContainer.orderCommendRepository.save(order);
+        testContainer.orderDetailCommendRepository.save(orderDetail);
+        testContainer.productRepository.save(product);
+
+        // when
+        ApiResponse<ConfirmCancelHttp.Response> result = testContainer.orderCancelController.confirmCancel(request);
+
+        // then
+        assertThat(orderDetail.getStatusCode()).isEqualTo(ORDER_CANCEL.getCode());
+        assertThat(orderDetail.getReason()).isEqualTo("단순변심");
+        assertThat(product.getStock()).isEqualTo(8L);
+
+        assertThat(result.getCode()).isEqualTo(OK.value());
+        assertThat(result.getStatus()).isEqualTo(OK);
+        assertThat(result.getMessage()).isEqualTo(OK.name());
+
+        assertThat(result.getData().getOrderId()).isEqualTo(1L);
+        assertThat(result.getData().getOrderNo()).isEqualTo("1");
+        assertThat(result.getData().getTotalPrice()).isEqualTo(3000L);
+        assertThat(result.getData().getCancellationDate()).isEqualTo(cancelledAt);
+
+        assertThat(result.getData().getCancelledItem().getProductId()).isEqualTo(1L);
+        assertThat(result.getData().getCancelledItem().getProductNo()).isEqualTo("1");
+        assertThat(result.getData().getCancelledItem().getName()).isEqualTo("빵빵이키링");
+        assertThat(result.getData().getCancelledItem().getPrice()).isEqualTo(1000L);
+        assertThat(result.getData().getCancelledItem().getQuantity()).isEqualTo(3L);
+
+        assertThat(result.getData().getRefundInfo().getTotalRefundAmount()).isEqualTo(3000L);
+        assertThat(result.getData().getRefundInfo().getRefundMethod()).isEqualTo("카드");
+        assertThat(result.getData().getRefundInfo().getRefundProcessedAt()).isEqualTo(this.now);
+    }
+
+    @Test
+    @DisplayName("부분 취소면 상품재고, 줄어든 수량이 변경된다.(Coupon)")
+    public void confirmCancel_Partial_Coupon() {
+        // given
+        ConfirmCancelHttp.Request request = ConfirmCancelHttp.Request.builder()
+                .orderDetailId(1L)
+                .numberOfCancellation(2L)
+                .reason("단순변심")
+                .build();
+
+        Order order = Order.builder()
+                .build();
+
+        Product product = Product.builder()
+                .name("빵빵이키링")
+                .productNo("1")
+                .stock(5L)
+                .price(1000L)
+                .build();
+
+        Coupon coupon = Coupon.builder()
+                .price(1000L)
+                .product(product)
+                .build();
+
+        MemberCoupon memberCoupon = MemberCoupon.builder()
+                .coupon(coupon)
+                .isUsed(true)
+                .usedDate(LocalDateTime.now())
+                .build();
+
+        LocalDateTime cancelledAt = LocalDateTime.of(2024, 2, 11, 3, 23);
+        OrderDetail orderDetail = OrderDetail.builder()
+                .memberCoupon(memberCoupon)
+                .order(order)
+                .product(product)
+                .quantity(3L)
+                .orderNo("1")
+                .price(1000L)
+                .reducedQuantity(0L)
+                .statusCode(ORDER_RECEIVED.getCode())
+                .cancelledAt(cancelledAt)
+                .build();
+
+        testContainer.orderCommendRepository.save(order);
+        testContainer.orderDetailCommendRepository.save(orderDetail);
+        testContainer.productRepository.save(product);
+
+        // when
+        ApiResponse<ConfirmCancelHttp.Response> result = testContainer.orderCancelController.confirmCancel(request);
+
+        // then
+        assertThat(memberCoupon.getIsUsed()).isFalse();
+        assertThat(memberCoupon.getUsedDate()).isNull();
+        assertThat(orderDetail.getStatusCode()).isEqualTo(ORDER_RECEIVED.getCode());
+        assertThat(orderDetail.getReason()).isNull();
+        assertThat(product.getStock()).isEqualTo(7L);
+
+        assertThat(result.getCode()).isEqualTo(OK.value());
+        assertThat(result.getStatus()).isEqualTo(OK);
+        assertThat(result.getMessage()).isEqualTo(OK.name());
+
+        assertThat(result.getData().getOrderId()).isEqualTo(1L);
+        assertThat(result.getData().getOrderNo()).isEqualTo("1");
+        assertThat(result.getData().getTotalPrice()).isEqualTo(2000L);
+        assertThat(result.getData().getCancellationDate()).isEqualTo(cancelledAt);
+
+        assertThat(result.getData().getCancelledItem().getProductId()).isEqualTo(1L);
+        assertThat(result.getData().getCancelledItem().getProductNo()).isEqualTo("1");
+        assertThat(result.getData().getCancelledItem().getName()).isEqualTo("빵빵이키링");
+        assertThat(result.getData().getCancelledItem().getPrice()).isEqualTo(1000L);
+        assertThat(result.getData().getCancelledItem().getQuantity()).isEqualTo(2L);
+
+        assertThat(result.getData().getRefundInfo().getTotalRefundAmount()).isEqualTo(2000L);
+        assertThat(result.getData().getRefundInfo().getRefundMethod()).isEqualTo("카드");
+        assertThat(result.getData().getRefundInfo().getRefundProcessedAt()).isEqualTo(this.now);
+    }
+
+    @Test
+    @DisplayName("모두 반품이면 상품재고, 줄어든 수량, 상태코드, 이유가 변경된다.(Coupon)")
+    public void confirmCancel_All_Coupon() {
+        // given
+        ConfirmCancelHttp.Request request = ConfirmCancelHttp.Request.builder()
+                .orderDetailId(1L)
+                .numberOfCancellation(3L)
+                .reason("단순변심")
+                .build();
+
+        Order order = Order.builder()
+                .build();
+
+        Product product = Product.builder()
+                .name("빵빵이키링")
+                .productNo("1")
+                .stock(5L)
+                .price(1000L)
+                .build();
+
+        Coupon coupon = Coupon.builder()
+                .price(1000L)
+                .product(product)
+                .build();
+
+        MemberCoupon memberCoupon = MemberCoupon.builder()
+                .coupon(coupon)
+                .isUsed(true)
+                .usedDate(LocalDateTime.now())
+                .build();
+
+        LocalDateTime cancelledAt = LocalDateTime.of(2024, 2, 11, 3, 23);
+        OrderDetail orderDetail = OrderDetail.builder()
+                .memberCoupon(memberCoupon)
+                .order(order)
+                .product(product)
+                .quantity(3L)
+                .orderNo("1")
+                .price(1000L)
+                .reducedQuantity(0L)
+                .statusCode(ORDER_RECEIVED.getCode())
+                .cancelledAt(cancelledAt)
+                .build();
+
+        testContainer.orderCommendRepository.save(order);
+        testContainer.orderDetailCommendRepository.save(orderDetail);
+        testContainer.productRepository.save(product);
+
+        // when
+        ApiResponse<ConfirmCancelHttp.Response> result = testContainer.orderCancelController.confirmCancel(request);
+
+        // then
+        assertThat(memberCoupon.getIsUsed()).isFalse();
+        assertThat(memberCoupon.getUsedDate()).isNull();
+        assertThat(orderDetail.getStatusCode()).isEqualTo(ORDER_CANCEL.getCode());
+        assertThat(orderDetail.getReason()).isEqualTo("단순변심");
+        assertThat(product.getStock()).isEqualTo(8L);
+
+        assertThat(result.getCode()).isEqualTo(OK.value());
+        assertThat(result.getStatus()).isEqualTo(OK);
+        assertThat(result.getMessage()).isEqualTo(OK.name());
+
+        assertThat(result.getData().getOrderId()).isEqualTo(1L);
+        assertThat(result.getData().getOrderNo()).isEqualTo("1");
+        assertThat(result.getData().getTotalPrice()).isEqualTo(3000L);
+        assertThat(result.getData().getCancellationDate()).isEqualTo(cancelledAt);
+
+        assertThat(result.getData().getCancelledItem().getProductId()).isEqualTo(1L);
+        assertThat(result.getData().getCancelledItem().getProductNo()).isEqualTo("1");
+        assertThat(result.getData().getCancelledItem().getName()).isEqualTo("빵빵이키링");
+        assertThat(result.getData().getCancelledItem().getPrice()).isEqualTo(1000L);
+        assertThat(result.getData().getCancelledItem().getQuantity()).isEqualTo(3L);
+
+        assertThat(result.getData().getRefundInfo().getTotalRefundAmount()).isEqualTo(3000L);
+        assertThat(result.getData().getRefundInfo().getRefundMethod()).isEqualTo("카드");
+        assertThat(result.getData().getRefundInfo().getRefundProcessedAt()).isEqualTo(this.now);
     }
 
     @Test
