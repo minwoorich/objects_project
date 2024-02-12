d0086ee8ad8549c514192af88329cbe6148daff7
Jhung Minwoo <minwoorich96@gmail.com>
2024-02-12 12:00:30 +0900

    [ :sparkles: | #301 | feature] response() 정의
    - 카카오 API를 이용해 결제조회

--- src/main/java/com/objects/marketbridge/payment/service/QuitPaymentService.java
+++ src/main/java/com/objects/marketbridge/payment/service/QuitPaymentService.java
@@ -1,5 +1,9 @@
 package com.objects.marketbridge.payment.service;
 
+import com.objects.marketbridge.common.config.KakaoPayConfig;
+import com.objects.marketbridge.common.dto.KakaoPayOrderRequest;
+import com.objects.marketbridge.common.dto.KakaoPayOrderResponse;
+import com.objects.marketbridge.common.infra.KakaoPayService;
 import com.objects.marketbridge.common.service.port.DateTimeHolder;
 import com.objects.marketbridge.order.domain.Order;
 import com.objects.marketbridge.order.service.port.OrderCommendRepository;
@@ -15,6 +19,8 @@ public class QuitPaymentService {
     private final OrderCommendRepository orderCommendRepository;
     private final OrderQueryRepository orderQueryRepository;
     private final DateTimeHolder dateTimeHolder;
+    private final KakaoPayService kakaoPayService;
+
 
     @Transactional
     public void cancel(String orderNo) {
@@ -27,5 +33,12 @@ public class QuitPaymentService {
 
         // 3) 주문, 상세주문들 softDelete
         orderCommendRepository.deleteByOrderNo(orderNo);
+    }
+
+    public KakaoPayOrderResponse response(String orderNo) {
+        Order order = orderQueryRepository.findByOrderNo(orderNo);
+        KakaoPayOrderRequest kakaoPayOrderRequest = KakaoPayOrderRequest.create(KakaoPayConfig.ONE_TIME_CID, order.getTid());
+
+        return kakaoPayService.getOrders(kakaoPayOrderRequest);
     }
 }
