3c0a548ee20cba7efb25d784d191cae4055b7bbe
Jhung Minwoo <minwoorich96@gmail.com>
2024-02-13 22:53:44 +0900

    [ :sparkles: | #315 | feature] createCart() : 이미 장바구니에 담긴 상품이면 예외 던지는 로직
    - ErrorCode 정의
    - 예외 던지는 코드 추가

--- src/main/java/com/objects/marketbridge/cart/controller/CartController.java
+++ src/main/java/com/objects/marketbridge/cart/controller/CartController.java
@@ -2,13 +2,20 @@ package com.objects.marketbridge.cart.controller;
 
 import com.objects.marketbridge.cart.controller.dto.CreateCartHttp;
 import com.objects.marketbridge.cart.service.CreateCartService;
+import com.objects.marketbridge.common.exception.exceptions.CustomLogicException;
+import com.objects.marketbridge.common.exception.exceptions.ErrorCode;
 import com.objects.marketbridge.common.interceptor.ApiResponse;
+import com.objects.marketbridge.common.security.annotation.AuthMemberId;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+import static com.objects.marketbridge.common.exception.exceptions.ErrorCode.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,8 +24,13 @@ public class CartController {
     private final CreateCartService createCartService;
 
     @PostMapping("/carts")
-    public ApiResponse<String> createCart(@RequestBody CreateCartHttp.Request request) {
-        createCartService.create();
+    public ApiResponse<String> createCart(
+            @RequestBody CreateCartHttp.Request request,
+            @AuthMemberId Long memberId) {
+        Boolean result = createCartService.create(request.toDto(memberId));
+        if(!result){
+            throw CustomLogicException.createBadRequestError(DUPLICATE_OPERATION, "이미 장바구니에 담긴 상품입니다", LocalDateTime.now());
+        }
         return ApiResponse.create();
     }
 }
--- src/main/java/com/objects/marketbridge/cart/service/CreateCartService.java
+++ src/main/java/com/objects/marketbridge/cart/service/CreateCartService.java
@@ -1,5 +1,6 @@
 package com.objects.marketbridge.cart.service;
 
+import com.objects.marketbridge.cart.service.dto.CreateCartDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -8,4 +9,8 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @RequiredArgsConstructor
 public class CreateCartService {
+
+    public Boolean create(CreateCartDto createCartDto) {
+
+    }
 }
