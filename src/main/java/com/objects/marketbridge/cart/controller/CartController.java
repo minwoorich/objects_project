6cdb7b3f9bd7d4f52f5d16fb4372ca81f4804524
Jhung Minwoo <minwoorich96@gmail.com>
2024-02-13 18:15:46 +0900

    [ :sparkles: | #315 | feature] createCart() 정의

--- src/main/java/com/objects/marketbridge/cart/controller/CartController.java
+++ src/main/java/com/objects/marketbridge/cart/controller/CartController.java
@@ -1,11 +1,24 @@
 package com.objects.marketbridge.cart.controller;
 
+import com.objects.marketbridge.cart.controller.dto.CreateCartHttp;
+import com.objects.marketbridge.cart.service.CreateCartService;
+import com.objects.marketbridge.common.interceptor.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
 public class CartController {
+    private final CreateCartService createCartService;
+
+    @PostMapping("/carts")
+    public ApiResponse<String> createCart(@RequestBody CreateCartHttp.Request request) {
+        createCartService.create();
+        return ApiResponse.create();
+    }
 }
