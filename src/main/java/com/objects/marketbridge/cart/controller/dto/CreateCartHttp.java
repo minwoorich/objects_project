4bc32944aff4efe9da8de3b6cc7bf736514e8992
Jhung Minwoo <minwoorich96@gmail.com>
2024-02-13 22:52:40 +0900

    [ :sparkles: | #316 | feature] CreateCartHttp.Request
     toDto() 메서드 작성 (CreateCartDto 생성하는 메서드)

--- src/main/java/com/objects/marketbridge/cart/controller/dto/CreateCartHttp.java
+++ src/main/java/com/objects/marketbridge/cart/controller/dto/CreateCartHttp.java
@@ -1,5 +1,6 @@
 package com.objects.marketbridge.cart.controller.dto;
 
+import com.objects.marketbridge.cart.service.dto.CreateCartDto;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,44 +12,27 @@ public class CreateCartHttp {
     public static class Request{
 
         private String productNo;
-        private String thumbImageUrl;
-        private String productName;
-        private String productOptionName;
-        private String deliveredDate; // 예상 도착시간(yyyy.MM.dd)
         private Long quantity;
-        private Long discountRate;
-        private Boolean isCouponAvailable;
-        private Long couponNo;
-        private Boolean isOwn;
         private Boolean isSubs;
 
+
         @Builder
-        private Request(String productNo, String thumbImageUrl, String productName, String productOptionName, String deliveredDate, Long quantity, Long discountRate, Boolean isCouponAvailable, Boolean isOwn, Boolean isSubs) {
+        private Request(String productNo, Long quantity,  Boolean isSubs) {
             this.productNo = productNo;
-            this.thumbImageUrl = thumbImageUrl;
-            this.productName = productName;
-            this.productOptionName = productOptionName;
-            this.deliveredDate = deliveredDate;
             this.quantity = quantity;
-            this.discountRate = discountRate;
-            this.isCouponAvailable = isCouponAvailable;
-            this.isOwn = isOwn;
             this.isSubs = isSubs;
         }
 
-        public static Request create(String productNo, String thumbImageUrl, String productName, String productOptionName, String deliveredDate, Long quantity, Long discountRate, Boolean isCouponAvailable, Boolean isOwn, Boolean isSubs) {
+        public static Request create(String productNo, Long quantity, Boolean isSubs) {
             return Request.builder()
                     .productNo(productNo)
-                    .thumbImageUrl(thumbImageUrl)
-                    .productName(productName)
-                    .productOptionName(productOptionName)
-                    .deliveredDate(deliveredDate)
                     .quantity(quantity)
-                    .discountRate(discountRate)
-                    .isCouponAvailable(isCouponAvailable)
-                    .isOwn(isOwn)
                     .isSubs(isSubs)
                     .build();
+        }
+
+        public CreateCartDto toDto(Long memberId) {
+            return CreateCartDto.create(productNo, memberId, quantity, isSubs);
         }
     }
 }
