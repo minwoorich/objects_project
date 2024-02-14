e7446781366c927a88986da20037aefd1fa93587
Minwoo Jhung <minwoorich@naver.com>
2024-02-14 17:56:20 +0900

    [ :white_check_mark: | #314 | test ] [CartControllerTest][테스트] POST /carts RestDocks 작성

--- src/main/java/com/objects/marketbridge/cart/controller/CartController.java
+++ src/main/java/com/objects/marketbridge/cart/controller/CartController.java
@@ -6,9 +6,12 @@ import com.objects.marketbridge.common.interceptor.ApiResponse;
 import com.objects.marketbridge.common.security.annotation.AuthMemberId;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequiredArgsConstructor
@@ -17,6 +20,7 @@ public class CartController {
     private final AddToCartService addToCartService;
 
     @PostMapping("/carts")
+    @ResponseStatus(HttpStatus.CREATED)
     public ApiResponse<String> addToCart(
             @RequestBody CreateCartHttp.Request request,
             @AuthMemberId Long memberId) {
