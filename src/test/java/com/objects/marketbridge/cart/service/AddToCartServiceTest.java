a8a73f4ba130e6365cf090ac41dbbab400070e2e
Jhung Minwoo <minwoorich96@gmail.com>
2024-02-14 14:12:59 +0900

    [ :white_check_mark:| #317 | test] [AddtoCartServiceTest]  add_outOfStock() 테스트 완료
    - add_outOfStock() : 재고 이상으로 장바구니에 담을경우 예외를 발생한다

--- src/test/java/com/objects/marketbridge/cart/service/AddToCartServiceTest.java
+++ src/test/java/com/objects/marketbridge/cart/service/AddToCartServiceTest.java
@@ -39,9 +39,9 @@ class AddToCartServiceTest {
     void init() {
 
         Product product1 = Product.create(null, true, "가방", 1000L, false, 100L, "thumbImg1", 0L, "productNo1");
-        Product product2 = Product.create(null, true, "신발", 1000L, false, 1L, "thumbImg1", 0L, "productNo2");
+        Product outOfStockProduct = Product.create(null, true, "신발", 1000L, false, 1L, "thumbImg1", 0L, "outOfStockProductNo");
         Product addedProduct = Product.create(null, true, "옷", 1000L, false, 100L, "thumbImg1", 0L, "addedProductNo");
-        productRepository.saveAll(List.of(product1, product2, addedProduct));
+        productRepository.saveAll(List.of(product1, outOfStockProduct, addedProduct));
 
         Member member = Member.create(MembershipType.BASIC.getText(), "test@email.com", "1234", "홍길동", "01012341234", true, true);
         memberRepository.save(member);
@@ -84,5 +84,20 @@ class AddToCartServiceTest {
         //then
         assertThat(thrown).isInstanceOf(CustomLogicException.class)
                 .hasFieldOrPropertyWithValue("errorCode", ErrorCode.DUPLICATE_OPERATION);
+    }
+
+    @DisplayName("재고 이상으로 장바구니에 담을경우 예외를 발생한다")
+    @Test
+    void add_outOfStock(){
+        //given
+        Member member = memberRepository.findByEmail("test@email.com");
+        CreateCartDto cartDto = CreateCartDto.create("outOfStockProductNo", member.getId(), 2L, false);
+
+        //when
+        Throwable thrown = catchThrowable(() -> addToCartService.add(cartDto));
+
+        //then
+        assertThat(thrown).isInstanceOf(CustomLogicException.class)
+                .hasFieldOrPropertyWithValue("errorCode", ErrorCode.OUT_OF_STOCK);
     }
 }
