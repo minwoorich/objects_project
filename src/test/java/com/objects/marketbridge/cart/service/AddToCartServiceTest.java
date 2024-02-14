4101f1d09758f6f45dc5abbc66741d7f0323e0f0
Jhung Minwoo <minwoorich96@gmail.com>
2024-02-14 14:08:56 +0900

    [ :white_check_mark:| #317 | test] [AddtoCartServiceTest]  add_validDuplicate() 테스트 완료
    - add_validDuplicate() : 이미 장바구니에 담긴 상품일 경우 예외를 발생한다

--- src/test/java/com/objects/marketbridge/cart/service/AddToCartServiceTest.java
+++ src/test/java/com/objects/marketbridge/cart/service/AddToCartServiceTest.java
@@ -5,6 +5,8 @@ import com.objects.marketbridge.cart.domain.Cart;
 import com.objects.marketbridge.cart.service.dto.CreateCartDto;
 import com.objects.marketbridge.cart.service.port.CartCommendRepository;
 import com.objects.marketbridge.cart.service.port.CartQueryRepository;
+import com.objects.marketbridge.common.exception.exceptions.CustomLogicException;
+import com.objects.marketbridge.common.exception.exceptions.ErrorCode;
 import com.objects.marketbridge.member.domain.Member;
 import com.objects.marketbridge.member.domain.MembershipType;
 import com.objects.marketbridge.member.service.port.MemberRepository;
@@ -37,12 +39,15 @@ class AddToCartServiceTest {
     void init() {
 
         Product product1 = Product.create(null, true, "가방", 1000L, false, 100L, "thumbImg1", 0L, "productNo1");
-        Product product2 = Product.create(null, true, "신발", 1000L, false, 100L, "thumbImg1", 0L, "productNo2");
-        Product product3 = Product.create(null, true, "옷", 1000L, false, 100L, "thumbImg1", 0L, "productNo3");
-        productRepository.saveAll(List.of(product1, product2, product3));
+        Product product2 = Product.create(null, true, "신발", 1000L, false, 1L, "thumbImg1", 0L, "productNo2");
+        Product addedProduct = Product.create(null, true, "옷", 1000L, false, 100L, "thumbImg1", 0L, "addedProductNo");
+        productRepository.saveAll(List.of(product1, product2, addedProduct));
 
         Member member = Member.create(MembershipType.BASIC.getText(), "test@email.com", "1234", "홍길동", "01012341234", true, true);
         memberRepository.save(member);
+
+        Cart cart = Cart.create(member, addedProduct, false, 2L);
+        cartCommendRepository.save(cart);
     }
     @AfterEach
     void clear() {
@@ -66,4 +71,18 @@ class AddToCartServiceTest {
         assertThat(cart.getProduct().getProductNo()).isEqualTo(cartDto.getProductNo());
     }
 
+    @DisplayName("이미 장바구니에 담긴 상품일 경우 예외를 발생한다")
+    @Test
+    void add_validDuplicate(){
+        //given
+        Member member = memberRepository.findByEmail("test@email.com");
+        CreateCartDto cartDto = CreateCartDto.create("addedProductNo", member.getId(), 2L, false);
+
+        //when
+        Throwable thrown = catchThrowable(() -> addToCartService.add(cartDto));
+
+        //then
+        assertThat(thrown).isInstanceOf(CustomLogicException.class)
+                .hasFieldOrPropertyWithValue("errorCode", ErrorCode.DUPLICATE_OPERATION);
+    }
 }
