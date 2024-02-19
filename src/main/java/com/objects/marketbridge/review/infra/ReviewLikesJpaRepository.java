29624adad36a6ee39714ea271dc40c88c6b1c1b5
Juho Lee <juho3416@gmail.com>
2024-02-19 12:03:04 +0900

    [ :wrench: | #238 | 리뷰-좋아요 관련 모두 주석처리 ]

--- src/main/java/com/objects/marketbridge/review/infra/ReviewLikesJpaRepository.java
+++ src/main/java/com/objects/marketbridge/review/infra/ReviewLikesJpaRepository.java
@@ -1,31 +1,32 @@
-package com.objects.marketbridge.review.infra;
-
-import com.objects.marketbridge.review.domain.ReviewLikes;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface ReviewLikesJpaRepository extends JpaRepository<ReviewLikes, Long> {
-
-    ReviewLikes findByReviewIdAndMemberId(Long reviewId, Long memberId);
-
-    Page<ReviewLikes> findAllByReview_Product_Id(Long productId, Pageable pageable);
-
-    Page<ReviewLikes> findAllByReview_Member_Id(Long memberId, Pageable pageable);
-
-    void delete(ReviewLikes reviewLikes);
-
-    void deleteById(Long reviewLikesId);
-
-    void deleteByReviewId(Long reviewId);
-
-    Long countByReviewIdAndLikedIsTrue(Long reviewId);
-
-    Boolean existsByReviewIdAndMemberId(Long reviewId, Long memberId);
-
-    List<ReviewLikes> findAllByReviewId(Long reviewId);
-
-    void deleteAllByReviewId(Long reviewId);
-}
+//package com.objects.marketbridge.review.infra;
+//
+//import com.objects.marketbridge.review.domain.ReviewLikes;
+//import org.springframework.data.domain.Page;
+//import org.springframework.data.domain.Pageable;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import java.util.List;
+//
+//public interface ReviewLikesJpaRepository extends JpaRepository<ReviewLikes, Long> {
+//
+//    ReviewLikes findByReviewIdAndMemberId(Long reviewId, Long memberId);
+//
+//    Page<ReviewLikes> findAllByReview_Product_Id(Long productId, Pageable pageable);
+//
+//    Page<ReviewLikes> findAllByReview_Member_Id(Long memberId, Pageable pageable);
+//
+//    void delete(ReviewLikes reviewLikes);
+//
+//    void deleteById(Long reviewLikesId);
+//
+//    void deleteByReviewId(Long reviewId);
+//
+//    Long countByReviewIdAndLikedIsTrue(Long reviewId);
+//
+//    Boolean existsByReviewIdAndMemberId(Long reviewId, Long memberId);
+//
+//    List<ReviewLikes> findAllByReviewId(Long reviewId);
+//
+//    void deleteAllByReviewId(Long reviewId);
+//}
+////LIKE관련//
