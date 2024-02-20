02878819590918e07dc9b8b073685efe19e81e66
Jhung Minwoo <minwoorich96@gmail.com>
2024-02-20 19:23:58 +0900

    [ :recycle:| #331 | refactor ] GetCartDto 수정
    - 필드에 cartId 추가

--- src/test/java/com/objects/marketbridge/cart/controller/CartControllerTest.java
+++ src/test/java/com/objects/marketbridge/cart/controller/CartControllerTest.java
@@ -18,7 +18,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.internal.stubbing.answers.DoesNothing;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
