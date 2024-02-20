2e64ef47043b577e89369f892fd759daea06f44d
yoon02 <sparkling_yo@naver.com>
2024-02-20 17:41:08 +0900

    [ :sparkles: | #271 |  feature ] 카테고리 기준 상품 조회 restdocs반영 및 controller 테스트 완료

--- src/test/java/com/objects/marketbridge/product/controller/docs/ProductControllerRestDocsTest.java
+++ src/test/java/com/objects/marketbridge/product/controller/docs/ProductControllerRestDocsTest.java
@@ -1,9 +1,13 @@
 package com.objects.marketbridge.product.controller.docs;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.objects.marketbridge.category.domain.Category;
 import com.objects.marketbridge.product.controller.ProductController;
 import com.objects.marketbridge.product.controller.request.CreateProductRequestDto;
+import com.objects.marketbridge.product.domain.Product;
 import com.objects.marketbridge.product.dto.ProductSimpleDto;
+import com.objects.marketbridge.product.mock.FakeCategoryRepository;
+import com.objects.marketbridge.product.mock.FakeProductRepository;
 import com.objects.marketbridge.product.service.ProductService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -26,14 +30,15 @@ import org.springframework.web.context.WebApplicationContext;
 
 import java.util.*;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.queryParameters;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -54,6 +59,12 @@ class ProductControllerRestDocsTest {
     @MockBean
     private ProductService productService;
 
+    @MockBean(name = "productRepository")
+    FakeProductRepository productRepository;
+    @MockBean(name = "categoryRepository")
+    FakeCategoryRepository categoryRepository;
+
+
     @BeforeEach
     public void setUp(WebApplicationContext webApplicationContext,
                       RestDocumentationContextProvider provider){
@@ -66,29 +77,131 @@ class ProductControllerRestDocsTest {
     @DisplayName("카테고리별 상품을 조회한다.")
     void getProductByCategory() throws Exception {
         //given
-        Pageable pageRequest = PageRequest.of(2,60);
-        Long categoryId = 236L;
-        ProductSimpleDto productSimpleDto = ProductSimpleDto.builder()
-                .productId(1L)
+        PageRequest pageRequest = PageRequest.of(0,5);
+
+        categoryRepository.save(Category.builder()
+                .parentId(2L)
+                .name("TEST CATE")
+                .level(1L)
+                .build());
+
+        Product product1 = Product.builder()
+                .id(1L)
+                .productNo("12343213-3423333")
                 .isOwn(false)
-                .stock(100L)
+                .price(10000L)
+                .name("TEST PRODUCT1")
+                .isSubs(true)
+                .stock(1000L)
+                .thumbImg("MAIN_IMG1.com")
                 .discountRate(12L)
-                .prodNo("1234566 - 434344")
-                .name("Test Product")
-                .thumUrl("mainImgurl.com")
+                .build();
+        product1.setCategory(categoryRepository.findById(1L));
+        productRepository.save(product1);
+
+        Product product2 = Product.builder()
+                .id(2L)
+                .isOwn(true)
+                .name("TEST PRODUCT2")
+                .stock(10030L)
+                .isSubs(false)
+                .price(1001L)
+                .thumbImg("MAIN_IMG111.com")
+                .discountRate(14L)
+                .productNo("12343213-3423434")
                 .build();
 
-        given(productService.getProductByCategory(any(Pageable.class),String.valueOf(anyLong())))
-                .willReturn(new PageImpl<>(Collections.singletonList(productSimpleDto),pageRequest,1));
+        product2.setCategory(categoryRepository.findById(1L));
+        productRepository.save(product2);
+
+        List<ProductSimpleDto> contents = List.of(ProductSimpleDto.of(product1),ProductSimpleDto.of(product2));
+
+        given(productService.getProductByCategory(any(PageRequest.class),anyString()))
+                .willReturn(new PageImpl<>(contents,pageRequest,2));
 
         //when //then
         mockMvc.perform(get("/product")
                         .param("size","60")
                         .param("page","1")
-                        .param("categoryCode",String.valueOf(categoryId))
+                        .param("categoryCode","1"
+                        )
                         .contentType(MediaType.APPLICATION_JSON))
                 .andDo(print())
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andDo(document("product-search-by-category",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        queryParameters(
+                                parameterWithName("size").description("한 페이지에 보여질 상품 갯수"),
+                                parameterWithName("page").description("조회 페이지 숫자"),
+                                parameterWithName("categoryCode").description("카테고리 코드 숫자, 해당 카테고리에 속하는 상품 목록 예) 여성패션=9")
+                        ),
+                        responseFields(
+                                fieldWithPath("code").type(JsonFieldType.NUMBER)
+                                        .description("코드"),
+                                fieldWithPath("status").type(JsonFieldType.STRING)
+                                        .description("상태"),
+                                fieldWithPath("message").type(JsonFieldType.STRING)
+                                        .description("메시지"),
+                                fieldWithPath("data").type(JsonFieldType.OBJECT)
+                                        .description("응답 데이터"),
+                                fieldWithPath("data.content[].productId").type(JsonFieldType.NUMBER)
+                                        .description("상품 아이디"),
+                                fieldWithPath("data.content[].prodNo").type(JsonFieldType.STRING)
+                                        .description("상품 고유 번호"),
+                                fieldWithPath("data.content[].name").type(JsonFieldType.STRING)
+                                        .description("상품 이름"),
+                                fieldWithPath("data.content[].price").type(JsonFieldType.NUMBER)
+                                        .description("상품 가격 (할인 전)"),
+                                fieldWithPath("data.content[].discountRate").type(JsonFieldType.NUMBER)
+                                        .description("상품 할인율"),
+                                fieldWithPath("data.content[].thumUrl").type(JsonFieldType.STRING)
+                                        .description("상품 썸네일 이미지 url"),
+                                fieldWithPath("data.content[].isOwn").type(JsonFieldType.BOOLEAN)
+                                        .description("마켓브릿지 상품인지 입점 판매자 상품인지 판별하는 값"),
+                                fieldWithPath("data.content[].stock").type(JsonFieldType.NUMBER)
+                                        .description("상품 재고"),
+
+                                fieldWithPath("data.pageable.pageNumber").type(JsonFieldType.NUMBER)
+                                        .description("페이지 번호"),
+                                fieldWithPath("data.pageable.pageSize").type(JsonFieldType.NUMBER)
+                                        .description("페이지당 요소 수"),
+                                fieldWithPath("data.pageable.sort.empty").type(JsonFieldType.BOOLEAN)
+                                        .description("정렬이 비어 있는지 여부"),
+                                fieldWithPath("data.pageable.sort.sorted").type(JsonFieldType.BOOLEAN)
+                                        .description("정렬이 되어 있는지 여부"),
+                                fieldWithPath("data.pageable.sort.unsorted").type(JsonFieldType.BOOLEAN)
+                                        .description("정렬이 되어 있지 않은지 여부"),
+                                fieldWithPath("data.pageable.offset").type(JsonFieldType.NUMBER)
+                                        .description("페이지 오프셋"),
+                                fieldWithPath("data.pageable.paged").type(JsonFieldType.BOOLEAN)
+                                        .description("페이지가 있는지 여부"),
+                                fieldWithPath("data.pageable.unpaged").type(JsonFieldType.BOOLEAN)
+                                        .description("페이지가 없는지 여부"),
+                                fieldWithPath("data.last").type(JsonFieldType.BOOLEAN)
+                                        .description("마지막 페이지 여부"),
+                                fieldWithPath("data.totalPages").type(JsonFieldType.NUMBER)
+                                        .description("전체 페이지 수"),
+                                fieldWithPath("data.totalElements").type(JsonFieldType.NUMBER)
+                                        .description("전체 요소 수"),
+                                fieldWithPath("data.size").type(JsonFieldType.NUMBER)
+                                        .description("페이지 크기"),
+                                fieldWithPath("data.number").type(JsonFieldType.NUMBER)
+                                        .description("페이지 번호"),
+                                fieldWithPath("data.sort.empty").type(JsonFieldType.BOOLEAN)
+                                        .description("정렬이 비어 있는지 여부"),
+                                fieldWithPath("data.sort.sorted").type(JsonFieldType.BOOLEAN)
+                                        .description("정렬이 되어 있는지 여부"),
+                                fieldWithPath("data.sort.unsorted").type(JsonFieldType.BOOLEAN)
+                                        .description("정렬이 되어 있지 않은지 여부"),
+                                fieldWithPath("data.first").type(JsonFieldType.BOOLEAN)
+                                        .description("첫 페이지 여부"),
+                                fieldWithPath("data.numberOfElements").type(JsonFieldType.NUMBER)
+                                        .description("현재 페이지의 요소 수"),
+                                fieldWithPath("data.empty").type(JsonFieldType.BOOLEAN)
+                                        .description("컨텐츠가 비어 있는지 여부")
+                        )
+                ));
     }
 
     @Test
