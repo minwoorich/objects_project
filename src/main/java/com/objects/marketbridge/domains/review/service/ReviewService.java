f8e448599f493cba29dee5f9340102e12108a43e
Juho Lee <juho3416@gmail.com>
2024-03-01 11:40:34 +0900

    [ :wrench: | #238 | 리뷰 관련 API 변경 ]

--- src/main/java/com/objects/marketbridge/domains/review/service/ReviewService.java
+++ src/main/java/com/objects/marketbridge/domains/review/service/ReviewService.java
@@ -17,7 +17,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
