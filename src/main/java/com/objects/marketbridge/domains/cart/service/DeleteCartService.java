5acad256d3d5265015eb136ed504e0570a9f2d7e
Jhung Minwoo <minwoorich96@gmail.com>
2024-03-25 13:15:47 +0900

    [ :recycle:| #544 | refactor ]
    - 가독성을 높이는 방향으로 리팩토링 진행

--- src/main/java/com/objects/marketbridge/domains/cart/service/DeleteCartService.java
+++ src/main/java/com/objects/marketbridge/domains/cart/service/DeleteCartService.java
@@ -4,12 +4,14 @@ import com.objects.marketbridge.domains.cart.service.port.CartCommendRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
+@Transactional
 public class DeleteCartService {
 
     private final CartCommendRepository cartCommendRepository;
