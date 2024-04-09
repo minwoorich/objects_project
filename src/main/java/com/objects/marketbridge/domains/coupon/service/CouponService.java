a5144762cca5524e7c16a033a83df145bace5b89
Jhung Minwoo <minwoorich96@gmail.com>
2024-04-09 18:47:42 +0900

    [ :recycle:| #589 | refactor ] CouponService 에 testability 를 높이기 위해 @Builder 추가 (@RequiredArgsConstructor 대신 생성자코드를 직접 작성해놓음)

--- src/main/java/com/objects/marketbridge/domains/coupon/service/CouponService.java
+++ src/main/java/com/objects/marketbridge/domains/coupon/service/CouponService.java
@@ -4,7 +4,7 @@ import com.objects.marketbridge.domains.coupon.controller.dto.GetCouponHttp;
 import com.objects.marketbridge.domains.coupon.domain.Coupon;
 import com.objects.marketbridge.domains.coupon.service.dto.GetCouponDto;
 import com.objects.marketbridge.domains.coupon.service.port.CouponRepository;
-import lombok.RequiredArgsConstructor;
+import lombok.Builder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,23 +14,27 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-@RequiredArgsConstructor
 @Slf4j
 @Transactional(readOnly = true)
 public class CouponService {
 
     private final CouponRepository couponRepository;
 
+    @Builder
+    public CouponService(CouponRepository couponRepository) {
+        this.couponRepository = couponRepository;
+    }
+
     public GetCouponHttp.Response findCouponsForProduct(Long productId) {
 
         List<Coupon> coupons = couponRepository.findByProductId(productId);
-        boolean haveCoupons = !coupons.isEmpty();
+        boolean hasCoupons = !coupons.isEmpty();
 
         List<GetCouponHttp.Response.CouponInfo> couponInfos = coupons.stream()
-                .map(c -> GetCouponDto.of(c, haveCoupons))
+                .map(c -> GetCouponDto.of(c, hasCoupons))
                 .map(GetCouponHttp.Response.CouponInfo::of)
                 .collect(Collectors.toList());
 
-        return GetCouponHttp.Response.create(haveCoupons, haveCoupons ? couponInfos : Collections.emptyList());
+        return GetCouponHttp.Response.create(hasCoupons, hasCoupons ? couponInfos : Collections.emptyList());
     }
 }
