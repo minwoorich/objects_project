3c58c66bcbaa4964aa20de893e372abf756e8e9e
Jhung Minwoo <minwoorich96@gmail.com>
2024-04-09 18:46:29 +0900

    [ :white_check_mark:| #589 | test ] CouponRepositoryImplTestWithFake 에 클렌징 코드 추가

--- src/test/java/com/objects/marketbridge/domains/coupon/infra/CouponRepositoryImplTestWithFake.java
+++ src/test/java/com/objects/marketbridge/domains/coupon/infra/CouponRepositoryImplTestWithFake.java
@@ -1,11 +1,13 @@
 package com.objects.marketbridge.domains.coupon.infra;
 
 import com.objects.marketbridge.domains.coupon.domain.Coupon;
+import com.objects.marketbridge.domains.coupon.mock.BaseFakeCouponRepository;
 import com.objects.marketbridge.domains.coupon.service.port.CouponRepository;
 import com.objects.marketbridge.domains.coupon.mock.FakeCouponRepository;
 import com.objects.marketbridge.domains.order.mock.FakeProductRepository;
 import com.objects.marketbridge.domains.product.domain.Product;
 import com.objects.marketbridge.domains.product.service.port.ProductRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -16,6 +18,13 @@ import static org.assertj.core.api.Assertions.*;
 class CouponRepositoryImplTestWithFake {
     CouponRepository couponRepository = new FakeCouponRepository();
     ProductRepository productRepository = new FakeProductRepository();
+
+    @AfterEach
+    void clear() {
+        BaseFakeCouponRepository.getInstance().clear();
+        productRepository.deleteAllInBatch();
+    }
+
 
     @DisplayName("상품아이디를 통해 상품에 등록된 모든 쿠폰들을 조회할 수 있다.")
     @Test
@@ -63,5 +72,38 @@ class CouponRepositoryImplTestWithFake {
         assertThat(coupons).isNotNull();
         assertThat(coupons).isInstanceOf(List.class);
         assertThat(coupons).isEmpty();
+    }
+
+    @DisplayName("DB에 저장된 모든 쿠폰을 조회 할 수 있다")
+    @Test
+    void findAll(){
+        //given
+        couponRepository.save(Coupon.builder().price(1000L).build());
+        couponRepository.save(Coupon.builder().price(2000L).build());
+       couponRepository.save(Coupon.builder().price(3000L).build());
+
+        //when
+        List<Coupon> coupons = couponRepository.findAll();
+
+        //then
+        assertThat(coupons).hasSize(3);
+        assertThat(coupons)
+                .extracting(c -> c.getPrice())
+                .containsExactly(1000L, 2000L, 3000L);
+    }
+
+    @DisplayName("상품에 등록된 쿠폰이 없을 경우 빈 List 객체를 반환한다.")
+    @Test
+    void findAll_empty(){
+        //given
+        // 아무것도 저장되어있지 않음
+
+        //when
+        List<Coupon> coupons = couponRepository.findAll();
+
+        //then
+        assertThat(coupons).isEmpty();
+        assertThat(coupons).isNotNull();
+        assertThat(coupons).isInstanceOf(List.class);
     }
 }
