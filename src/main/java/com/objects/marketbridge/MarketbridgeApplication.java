36ceb2b3dcca625b2d44f0a532f0bca63664036a
Minwoo Jhung <minwoorich@naver.com>
2024-04-10 19:17:01 +0900

    [ :recycle: | #592 | fix] 타임존 에러 고치기
     - MarketbridgeApplication 클래스에@PostConstruct 를 사용해   전역적으로 시간대를 KST로 설정

--- src/main/java/com/objects/marketbridge/MarketbridgeApplication.java
+++ src/main/java/com/objects/marketbridge/MarketbridgeApplication.java
@@ -1,10 +1,13 @@
 package com.objects.marketbridge;
 
 import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 //@EnableJpaAuditing -> JpaAuditingConfig.class 파일로 (common/config) 따로 분리하였습니다 (컨트롤러 테스트시 문제발생)
 // [주의] 만일 @SpringBootTest 대신 @DataJpaTest 을 사용하신다면
@@ -19,5 +22,10 @@ public class MarketbridgeApplication {
 	@Bean
 	Hibernate5JakartaModule hibernate5Module() {
 		return new Hibernate5JakartaModule();
+	}
+
+	@PostConstruct
+	public void setTime() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 }
