bcef15a70c634e20270919d00679d2e29c0a0c29
Jhung Minwoo <minwoorich96@gmail.com>
2024-04-16 11:47:04 +0900

    [ :white_check_mark: | #544 | refactor] docs 요청할 때마다 로그남는문제 해결
    - LoggingFilter 화이트리스트에 docs 추가

--- src/main/java/com/objects/marketbridge/common/logging/LoggingFilter.java
+++ src/main/java/com/objects/marketbridge/common/logging/LoggingFilter.java
@@ -36,18 +36,28 @@ public class LoggingFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException,IOException {
 
-        if (!whiteList.contains(request.getRequestURI())) {
-
-            String uuid = UUID.randomUUID().toString();
-            MDC.put("traceId", uuid.substring(0,7));
-
-            if (isAsyncDispatch(request)) {
-                filterChain.doFilter(request, response);
-            } else {
-                doFilterWrapped(new RequestWrapper(request), new ResponseWrapper(response), filterChain);
-            }
-            MDC.clear();
+        if (isWhiteListed(request)) {
+            // 화이트리스트에 포함된 요청은 로깅 없이 다음 필터 또는 서블릿으로 전달
+            filterChain.doFilter(request, response);
+        } else {
+            proceedWithLogging(request, response, filterChain);
         }
+    }
+
+    private void proceedWithLogging(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        String uuid = UUID.randomUUID().toString();
+        MDC.put("traceId", uuid.substring(0,7));
+
+        if (isAsyncDispatch(request)) {
+            filterChain.doFilter(request, response);
+        } else {
+            doFilterWrapped(new RequestWrapper(request), new ResponseWrapper(response), filterChain);
+        }
+        MDC.clear();
+    }
+
+    private boolean isWhiteListed(HttpServletRequest request) {
+        return whiteList.contains(request.getRequestURI());
     }
 
     protected void doFilterWrapped(RequestWrapper request, ContentCachingResponseWrapper response, FilterChain filterChain) throws ServletException, IOException {
